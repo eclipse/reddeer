c2c9bc7183dd2d06b33a28b2d99ff39b5ade264c
rhopp <rhopp@redhat.com>
2012-08-27 11:25:37 +0200

    First draft of NewJavaProjectWizardPage

---
+++ org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/jdt/ui/ide/NewJavaProjectWizardPage.java
@@ -0,0 +1,41 @@
+package org.jboss.reddeer.eclipse.jdt.ui.ide;
+
+import org.jboss.reddeer.eclipse.jface.wizard.WizardDialog;
+import org.jboss.reddeer.eclipse.jface.wizard.WizardPage;
+import org.jboss.reddeer.swt.impl.button.CheckBox;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+
+/**
+ * Represents first page of New Java Project Wizard
+ * 
+ * @author rhopp
+ *
+ */
+
+public class NewJavaProjectWizardPage extends WizardPage {
+
+	protected NewJavaProjectWizardPage(WizardDialog wizardDialog,
+			int pageIndex) {
+		super(wizardDialog, 1);		
+	}
+	
+	public void setProjectName (String projectName){
+	    show();
+	    log.debug("Set General Project name to " + projectName);
+	    new LabeledText("Project name:").setText(projectName);
+	}
+	
+	public void useDefaultLocation(boolean check){
+		show();
+		CheckBox box = new CheckBox("Use default location");
+		log.debug("Setting default location to "+check);
+		box.toggle(check);
+	}
+	
+	public void setLocation(String location){
+		log.debug("Setting Location to "+location);
+		LabeledText text = new LabeledText("Location:");
+		text.setText(location);
+	}
+
+}
