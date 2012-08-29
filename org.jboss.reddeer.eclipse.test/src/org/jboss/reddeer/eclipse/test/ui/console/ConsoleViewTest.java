211bd6daa8728121f115869dd7cb3d7b1ead88da
jjankovi <jjankovi@redhat.com>
2012-08-29 09:05:11 +0200

    enhancing of ConsoleViewTest

--- org.jboss.reddeer.eclipse.test/src/org/jboss/reddeer/eclipse/test/ui/console/ConsoleViewTest.java
+++ org.jboss.reddeer.eclipse.test/src/org/jboss/reddeer/eclipse/test/ui/console/ConsoleViewTest.java
@@ -2,28 +2,89 @@ package org.jboss.reddeer.eclipse.test.ui.console;
 
 import static org.junit.Assert.assertThat;
 
+import org.hamcrest.core.IsEqual;
 import org.hamcrest.core.IsNull;
+import org.jboss.reddeer.eclipse.jdt.ui.ide.NewJavaProjectWizardDialog;
+import org.jboss.reddeer.eclipse.jdt.ui.ide.NewJavaProjectWizardPage;
+import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.PackageExplorer;
 import org.jboss.reddeer.eclipse.ui.console.ConsoleView;
+import org.jboss.reddeer.eclipse.ui.ide.NewJavaClassDialog;
+import org.jboss.reddeer.swt.impl.menu.ShellMenu;
+import org.jboss.reddeer.swt.matcher.RegexMatchers;
+import org.jboss.reddeer.swt.util.Bot;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ConsoleViewTest {
 
 	private ConsoleView consoleView;
 	
-	@Test
-	public void open() {
+	private static final String TEST_PROJECT_NAME = "Project";
+	
+	@Before
+	public void setup() {
 		consoleView = new ConsoleView();
 		consoleView.open();
+		runTestProject();
+	}
+	
+	@AfterClass
+	public static void tearDown() {
+		new PackageExplorer().deleteItem(TEST_PROJECT_NAME, true);
 	}
 	
 	@Test
-	public void getText() {
-		String text = new ConsoleView().getConsoleText();
-		assertThat(text, IsNull.nullValue());
+	public void getConsoleText() {
+		consoleView = new ConsoleView();
+		String text = consoleView.getConsoleText();
+		assertThat(text, IsNull.notNullValue());
+		assertThat(text, IsEqual.equalTo("Hello World"));
 	}
 	
 	@Test
-	public void clearText() {
-		new ConsoleView().clearConsole();
+	public void clearConsole() {
+		consoleView = new ConsoleView();
+		consoleView.clearConsole();
+		String text = consoleView.getConsoleText();
+		assertThat(text, IsEqual.equalTo(""));
+	}
+	
+	private void runTestProject() {
+		createTestProject();
+		RegexMatchers m = new RegexMatchers("Run.*", "Run As.*", ".*Java Application.*");
+		new ShellMenu(m.getMatchers()).select();
+	}
+	
+	private void createTestProject() {
+		PackageExplorer packageExplorer = new PackageExplorer();
+		if (!packageExplorer.contains(TEST_PROJECT_NAME)) {
+			createJavaProject();
+			createJavaClass();
+		}
+		packageExplorer.selectItem(TEST_PROJECT_NAME);
+	}
+	
+	private void createJavaProject() {
+		NewJavaProjectWizardDialog javaProject = new NewJavaProjectWizardDialog();
+		javaProject.open();
+		
+		NewJavaProjectWizardPage javaWizardPage = new NewJavaProjectWizardPage(javaProject);
+		javaWizardPage.setProjectName(TEST_PROJECT_NAME);
+		
+		javaProject.finish(false);
+	}
+	
+	private void createJavaClass() {
+		NewJavaClassDialog javaClassDialog = new NewJavaClassDialog();
+		javaClassDialog.open();
+		javaClassDialog.setName("TestClass");
+		javaClassDialog.setPackage("test");
+		javaClassDialog.setStaticMainMethod(true);
+		javaClassDialog.finish();
+		
+		Bot.get().activeEditor().toTextEditor().
+			insertText(9, 0, "System.out.print(\"Hello World\");");
+		Bot.get().activeEditor().toTextEditor().save();
 	}
 }
--- org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/ui/ide/NewJavaClassDialog.java
+++ org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/ui/ide/NewJavaClassDialog.java
@@ -1,8 +1,7 @@
 package org.jboss.reddeer.eclipse.ui.ide;
 
-import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.PackageExplorer;
 import org.jboss.reddeer.eclipse.jface.wizard.NewWizardDialog;
-import org.jboss.reddeer.eclipse.jface.wizard.WizardDialog;
+import org.jboss.reddeer.swt.impl.button.CheckBox;
 import org.jboss.reddeer.swt.impl.text.LabeledText;
 
 public class NewJavaClassDialog extends NewWizardDialog {
@@ -15,4 +14,12 @@ public class NewJavaClassDialog extends NewWizardDialog {
 		new LabeledText("Name:").setText(name);
 	}
 	
+	public void setPackage(String packageName) {
+		new LabeledText("Package:").setText(packageName);
+	}
+	
+	public void setStaticMainMethod(boolean setMainMethod) {
+//		new CheckBox("public static void main(String[] args)").toggle(setMainMethod);
+		new CheckBox(4).toggle(setMainMethod); // initiate with label doesnt work
+	}
 }
