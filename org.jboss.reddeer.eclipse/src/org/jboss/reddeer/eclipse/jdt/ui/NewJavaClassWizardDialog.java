09422de7758f87ada31e505b8550acd4a3bd57d0
jjankovi <jjankovi@redhat.com>
2012-10-24 10:40:52 +0200

    commit forgotten NewJavaClassWizard classes moved into correct location

---
+++ org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/jdt/ui/NewJavaClassWizardDialog.java
@@ -0,0 +1,15 @@
+package org.jboss.reddeer.eclipse.jdt.ui;
+
+import org.jboss.reddeer.eclipse.jface.wizard.NewWizardDialog;
+
+public class NewJavaClassWizardDialog extends NewWizardDialog {
+	
+	public NewJavaClassWizardDialog() {
+		super("Java", "Class");
+	}
+	
+	@Override
+	public NewJavaClassWizardPage getFirstPage() {
+		return new NewJavaClassWizardPage(this);
+	}
+}
