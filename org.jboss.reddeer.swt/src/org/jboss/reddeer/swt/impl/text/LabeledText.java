fdf06a9089bca6af500869852018585dc219768c
rawagner <rawagner@redhat.com>
2013-03-05 11:33:43 +0100

    Added new constructor to LabeledText

--- org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/impl/text/LabeledText.java
+++ org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/impl/text/LabeledText.java
@@ -1,6 +1,8 @@
 package org.jboss.reddeer.swt.impl.text;
 
+import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.jboss.reddeer.swt.api.Text;
+import org.jboss.reddeer.swt.exception.SWTLayerException;
 import org.jboss.reddeer.swt.util.Bot;
 
 /**
@@ -15,7 +17,24 @@ public class LabeledText extends AbstractText implements Text {
 	 * @param label
 	 */
 	public LabeledText(String label) {
-		botText = Bot.get().textWithLabel(label);
+		try{
+			botText = Bot.get().textWithLabel(label);
+		} catch (WidgetNotFoundException e){
+			throw new SWTLayerException("Text with label " + label + " was not found");
+		}
+	}
+	
+	/**
+	 * Text with given label in given Group
+	 * @param label of text
+	 * @param group in group
+	 */
+	public LabeledText(String label, String group){
+		try{
+			botText = Bot.get().textWithLabelInGroup(label, group);
+		} catch (WidgetNotFoundException e){
+			throw new SWTLayerException("Text with label " + label + " in group "+group+" was not found");
+		}
 	}
 	
 }
