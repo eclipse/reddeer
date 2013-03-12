a3d9d2c55710cca9cf8524bffdcd1ff0244e31a2
jjankovi <jjankovi@redhat.com>
2013-03-12 17:01:32 +0100

    ShellToolBar and ShellToolItem implemented + tests covering this new objects

---
+++ org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/impl/toolbar/ShellToolBar.java
@@ -0,0 +1,19 @@
+package org.jboss.reddeer.swt.impl.toolbar;
+
+import org.jboss.reddeer.swt.lookup.impl.ToolBarLookup;
+
+/**
+ * ShellToolBar implementation. It expects shell where toolbar should be found on
+ * @author jjankovi
+ *
+ */
+public class ShellToolBar extends AbstractToolBar {
+
+	/**
+	 * Lookup for ShellToolBar
+	 */
+	public ShellToolBar() {
+		toolBar = new ToolBarLookup().getShellToolBars();
+	}
+	
+}
