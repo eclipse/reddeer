897f9937f6f5a84ed6bed925b9fd3830d0712221
Jiri Peterka <peterka.jiri@gmail.com>
2013-04-18 19:08:09 +0200

    Improved logging removed redundant log4 conf files and added debug configurable

--- org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/RedDeerTest.java
+++ org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/RedDeerTest.java
@@ -1,5 +1,9 @@
 package org.jboss.reddeer.swt.test;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
 import org.jboss.reddeer.swt.util.Bot;
@@ -8,13 +12,31 @@ import org.junit.Before;
 /**
  * Parent test for each test of Red Deer
  * @author Vlado Pakan
- *
+ * @author Jiri Peterka
  */
 public class RedDeerTest {
+	
 	@Before
 	public void setUpRDT(){
+		configureLogging();
 		setUp();
 	}
+	
+	private void configureLogging() {
+		ConsoleAppender console = new ConsoleAppender();
+		String PATTERN = "%-5p [%t][%C{1}] %m%n";
+		console.setLayout(new PatternLayout(PATTERN));
+		// if you want to enable just add vm argument -Dlog.debug=true
+		if (System.getProperty("log.debug").equalsIgnoreCase("true")) {
+			console.setThreshold(Level.DEBUG);
+		}
+		else {
+			console.setThreshold(Level.INFO);
+		}
+		console.activateOptions();
+		Logger.getRootLogger().addAppender(console);
+	}
+
 	@After
 	public void tearDownRDT(){
 		tearDown();
