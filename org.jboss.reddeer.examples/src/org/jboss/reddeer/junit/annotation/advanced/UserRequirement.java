cac046a743135a5c3caca9a8e061f10c4d1b9b00
vpakan <vpakan@redhat.com>
2013-04-19 14:31:24 +0200

    Refactores wiki.examples to examples and fixes sample tests

---
+++ org.jboss.reddeer.examples/src/org/jboss/reddeer/junit/annotation/advanced/UserRequirement.java
@@ -0,0 +1,38 @@
+package org.jboss.reddeer.junit.annotation.advanced;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.jboss.reddeer.junit.requirement.Requirement;
+import org.jboss.reddeer.junit.annotation.advanced.UserRequirement.User;
+/**
+ * Parameterized requirement with paramer name 
+ * @author vpakan
+ *
+ */
+public class UserRequirement implements Requirement<User> {
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.TYPE)
+	public @interface User {
+		String name();
+	}
+	
+	private User user;
+	
+	public boolean canFulfill() {
+		// return true if you can connect to the database
+		return true;
+	}
+
+	public void fulfill() {
+		System.out.println("Fulfilling reuirement User with name: " + user.name());
+		// create an admin user in the database if it does not exist yet
+	}
+	
+	public void setDeclaration(User user) {
+		this.user = user;
+	}
+}
---
+++ org.jboss.reddeer.examples/src/org/jboss/reddeer/junit/configuration/simple/UserTest.java
@@ -0,0 +1,23 @@
+package org.jboss.reddeer.junit.configuration.simple;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.junit.configuration.simple.UserRequirement.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+/**
+ * Test with User requirement using configuration from simple xml file
+ * 
+ * Set VM parameter -Dreddeer.config to point to directory with requirements.xml file 
+ * -Dreddeer.config=${project_loc}/src/org/jboss/reddeer/junit/configuration/simple
+ * 
+ * @author lucia jelinkova
+ *
+ */
+@RunWith(RedDeerSuite.class)
+@User
+public class UserTest {
+	@Test
+	public void test(){
+		// put test logic here
+	}
+}
