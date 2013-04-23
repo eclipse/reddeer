7f2b0749f9fc2e1a1502d9e5595d152055c4498f
sbunciak <sbunciak@redhat.com>
2013-04-23 14:58:11 +0200

    Fix for RedDeer requirements to work with UI runner

--- org.jboss.reddeer.junit/src/org/jboss/reddeer/junit/internal/configuration/entity/PropertyBasedConfiguration.java
+++ org.jboss.reddeer.junit/src/org/jboss/reddeer/junit/internal/configuration/entity/PropertyBasedConfiguration.java
@@ -7,55 +7,97 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.eclipse.core.runtime.Platform;
 import org.jboss.reddeer.junit.configuration.RedDeerConfigurationException;
 import org.jboss.reddeer.junit.requirement.Requirement;
 
 /**
- * JAXB annotated class for reading the default (property based) configuration. 
+ * JAXB annotated class for reading the default (property based) configuration.
  * 
  * @author Lucia Jelinkova
- *
+ * @author sbunciak
  */
-@XmlRootElement(name="requirement", namespace="http://www.jboss.org/NS/Req")
+@XmlRootElement(name = "requirement", namespace = "http://www.jboss.org/NS/Req")
 public class PropertyBasedConfiguration {
 
 	private List<Property> properties = new ArrayList<Property>();
-	
+
 	private String clazz;
-	
+
 	private Class<? extends Requirement<?>> className;
-	
+
+	/**
+	 * @param List of requirement properties to be set
+	 */
 	public void setProperties(List<Property> properties) {
 		this.properties = properties;
 	}
-	
-	@XmlElement(name="property", namespace="http://www.jboss.org/NS/Req")
+
+	/**
+	 * @return List of requirement properties 
+	 */
+	@XmlElement(name = "property", namespace = "http://www.jboss.org/NS/Req")
 	public List<Property> getProperties() {
 		return properties;
 	}
-	
+
+	/**
+	 * @param Requirement class name 
+	 */
 	public void setRequirementClassName(String clazz) {
 		this.clazz = clazz;
 	}
-	
-	@XmlAttribute(name="class")
+
+	/**
+	 * @return Class name of the requirement 
+	 */
+	@XmlAttribute(name = "class")
 	public String getRequirementClassName() {
 		return clazz;
 	}
-	
+
+	/**
+	 * @return Requirement class looked up in running Eclipse/OSGi
+	 * @since 0.2
+	 */
 	public Class<? extends Requirement<?>> getRequirementClass() {
-		if (className == null){
+		if (className == null) {
 			className = createClass(getRequirementClassName());
 		}
 		return className;
 	}
-	
-	@SuppressWarnings("unchecked")
+
+	/*
+	 * Search Eclipse Platform command line arguments for test plugin (bundle)
+	 * where test classes are looked up
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private Class<? extends Requirement<?>> createClass(String name) {
 		try {
-			return (Class<? extends Requirement<?>>) Class.forName(name);
+
+			String testPlugin = "";
+			String[] args = Platform.getCommandLineArgs();
+			for (int i = 0; i < args.length; i++) {
+				System.out.println(args[i]);
+				if (args[i].contains("testpluginname")) {
+					testPlugin = args[i + 1];
+				}
+			}
+
+			Class clazz1;
+			try {
+				// load class from Eclipse bundle (if running as RedDeer Test)
+				clazz1 = Platform.getBundle(testPlugin).loadClass(name);
+			} catch (NullPointerException e) {
+				// load class from junit classpath (if running as JUnit test)
+				clazz1 = Class.forName(name);
+			}
+			
+			return (Class<? extends Requirement<?>>) clazz1;
 		} catch (ClassNotFoundException e) {
-			throw new RedDeerConfigurationException("The requirement class defined in the XML configuration file cannot be created. See details below", e);
+			e.printStackTrace();
+			throw new RedDeerConfigurationException(
+					"The requirement class defined in the XML configuration file cannot be created. See details below",	e);
 		}
 	}
 }
