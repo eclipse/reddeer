3f0b13ca77400acb0ec63d9d5cb8b99f69d3b32b
vpakan <vpakan@redhat.com>
2013-07-30 15:34:42 +0200

    Adds Custom TimePeriod

--- org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/wait/TimePeriod.java
+++ org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/wait/TimePeriod.java
@@ -2,13 +2,14 @@ package org.jboss.reddeer.swt.wait;
 
 /**
  * Represents the time scale of how long the user operation might last.
- * 
+ * CUSTOM TimePeriod has to be created calling static method TimePeriod.getCustom(long seconds)
+ * with positive input parameter
  * @author Lucia Jelinkova
  *
  */
 public enum TimePeriod {
 
-	NORMAL(10), LONG(60), VERY_LONG(300);
+	NORMAL(10), LONG(60), VERY_LONG(300), CUSTOM(-1);
 	
 	private long seconds;
 	
@@ -17,6 +18,16 @@ public enum TimePeriod {
 	}
 	
 	public long getSeconds(){
+	  if (this.equals(TimePeriod.CUSTOM) && seconds == -1){
+	    throw new UnsupportedOperationException("CUSTOM TimePeriod has to be created calling static method "
+	        + "TimePeriod.getCustom(long seconds) with positive input parameter");
+	  }
 		return seconds;
+	}
+	
+	public static TimePeriod getCustom(long seconds){
+	  TimePeriod custom = TimePeriod.CUSTOM;
+	  custom.seconds = seconds;
+	  return custom;
 	}
 }
