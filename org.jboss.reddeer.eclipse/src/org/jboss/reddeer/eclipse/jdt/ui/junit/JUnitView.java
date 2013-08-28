320304b6ecdd4a72106b87132e572e90938e9a8b
Andrej Podhradsky <apodhrad@redhat.com>
2013-08-28 17:11:44 +0200

    REDDEER-56: JUnitView initial implementation

---
+++ org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/jdt/ui/junit/JUnitView.java
@@ -0,0 +1,57 @@
+package org.jboss.reddeer.eclipse.jdt.ui.junit;
+
+import org.jboss.reddeer.swt.impl.label.DefaultLabel;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+import org.jboss.reddeer.workbench.view.View;
+
+/**
+ * JUnit View
+ * 
+ * @author apodhrad
+ * 
+ */
+public class JUnitView extends View {
+
+	public JUnitView() {
+		super("JUnit");
+	}
+
+	/**
+	 * Returns run status in the form x/y where x is the number of executed
+	 * tests and y is the number of all tests.
+	 * 
+	 * @return run status
+	 */
+	public String getRunStatus() {
+		return new LabeledText("Runs: ").getText().trim();
+	}
+
+	/**
+	 * Returns the number of errors.
+	 * 
+	 * @return number of errors
+	 */
+	public int getNumberOfErrors() {
+		String errorStatus = new LabeledText("Errors: ").getText().trim();
+		return Integer.valueOf(errorStatus);
+	}
+
+	/**
+	 * Returns the number of failures.
+	 * 
+	 * @return number of failures
+	 */
+	public int getNumberOfFailures() {
+		String errorStatus = new LabeledText("Failures: ").getText().trim();
+		return Integer.valueOf(errorStatus);
+	}
+
+	/**
+	 * Returns the view status.
+	 * 
+	 * @return view status
+	 */
+	public String getViewStatus() {
+		return new DefaultLabel().getText().trim();
+	}
+}
