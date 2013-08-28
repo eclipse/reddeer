3b3536adad2b78561d02ecfc06b7e3e80652e9d8
Jiri Peterka <peterka.jiri@gmail.com>
2013-08-28 09:46:07 +0200

    Bot class marked as deprecated (it will be removed in 0.4)

--- org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/util/Bot.java
+++ org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/util/Bot.java
@@ -3,9 +3,11 @@ package org.jboss.reddeer.swt.util;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 
 /**
- * Bot provider for all redder
+ * SWTBot provider for RedDeer
  * @author Jiri Peterka
- *
+ * @deprecated please avoid using this instance because it will be removed in 0.4
+ * if you need SWTBot in your test, please instantiate it yourself (new SWTWorkbenchBot()) 
+ * or clone this class. RedDeer code should avoid using this class at all. 
  */
 public class Bot {
 	/**
