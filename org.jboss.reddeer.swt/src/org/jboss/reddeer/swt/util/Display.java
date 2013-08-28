ea10e3be41e7924a7e820e270d3336cb0c7ba071
Jiri Peterka <peterka.jiri@gmail.com>
2013-08-28 16:25:31 +0200

    Display class rewritten to be SWTBot independent

--- org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/util/Display.java
+++ org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/util/Display.java
@@ -3,7 +3,6 @@ package org.jboss.reddeer.swt.util;
 import java.util.ArrayList;
 
 import org.eclipse.swt.SWTException;
-import org.eclipse.swtbot.swt.finder.utils.SWTUtils;
 import org.jboss.reddeer.swt.exception.SWTLayerException;
 
 /**
@@ -13,9 +12,11 @@ import org.jboss.reddeer.swt.exception.SWTLayerException;
  */
 public class Display {
 	
+	private static org.eclipse.swt.widgets.Display display;
+	
 	public static void syncExec(Runnable runnable) {
 		try{
-			SWTUtils.display().syncExec(runnable);
+			getDisplay().syncExec(runnable);
 		}catch(SWTException ex){
 			if(ex.getCause() instanceof SWTLayerException){
 				throw (SWTLayerException) ex.getCause();
@@ -27,7 +28,7 @@ public class Display {
 
 	public static void asyncExec(Runnable runnable) {
 		try{
-			SWTUtils.display().asyncExec(runnable);
+			getDisplay().asyncExec(runnable);
 		}catch(SWTException ex){
 			if(ex.getCause() instanceof SWTLayerException){
 				throw (SWTLayerException) ex.getCause();
@@ -36,10 +37,24 @@ public class Display {
 			}
 		}
 	}
-		
-	
+
+	/**
+	 * Returns org.eclipse.swt.widgets.Display instance. If now know it tries to get in available threads.   
+	 * @return current Display instance or SWTLayerException if not found
+	 */
 	public static org.eclipse.swt.widgets.Display getDisplay() {
-		return SWTUtils.display();
+		if ((display == null) || display.isDisposed()) {
+			display = null;
+			Thread[] allThreads = allThreads();
+			for (Thread thread : allThreads) {
+				org.eclipse.swt.widgets.Display d = org.eclipse.swt.widgets.Display.findDisplay(thread);
+				if (d != null)
+					display = d;
+			}
+			if (display == null)
+				throw new SWTLayerException("Could not find a display");
+		}
+		return display;
 	}
 
 	/**
@@ -96,6 +111,26 @@ public class Display {
 		}
 		return list.get(0);
 		
+	}
+	
+	
+	private static Thread[] allThreads() {
+		ThreadGroup threadGroup = primaryThreadGroup();
+
+		Thread[] threads = new Thread[64];
+		int enumerate = threadGroup.enumerate(threads, true);
+
+		Thread[] result = new Thread[enumerate];
+		System.arraycopy(threads, 0, result, 0, enumerate);
+
+		return result;
+	}
+
+	private static ThreadGroup primaryThreadGroup() {
+		ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
+		while (threadGroup.getParent() != null)
+			threadGroup = threadGroup.getParent();
+		return threadGroup;
 	}
 	
 }
