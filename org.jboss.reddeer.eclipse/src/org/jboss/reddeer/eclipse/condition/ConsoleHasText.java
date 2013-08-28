af645acd49f7b06f3035406b27ed56467c955fe0
Andrej Podhradsky <apodhrad@redhat.com>
2013-08-28 15:59:44 +0200

    REDDEER-265: Create condition ConsoleHasText

---
+++ org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/condition/ConsoleHasText.java
@@ -0,0 +1,38 @@
+package org.jboss.reddeer.eclipse.condition;
+
+import org.jboss.reddeer.eclipse.ui.console.ConsoleView;
+import org.jboss.reddeer.swt.condition.WaitCondition;
+
+/**
+ * Returns true if the console contains a given text
+ * 
+ * @author apodhrad
+ * 
+ */
+public class ConsoleHasText implements WaitCondition {
+
+	private String text;
+
+	public ConsoleHasText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public boolean test() {
+		String consoleText = getConsoleText();
+		return consoleText.contains(text);
+	}
+
+	@Override
+	public String description() {
+		String consoleText = getConsoleText();
+		return "Console doesn't contain '" + text + "'\n" + consoleText;
+	}
+
+	private static String getConsoleText() {
+		ConsoleView consoleView = new ConsoleView();
+		consoleView.open();
+		return consoleView.getConsoleText();
+	}
+
+}
