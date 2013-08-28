330419823c5128565faf928beff85765c6989909
Jiri Peterka <peterka.jiri@gmail.com>
2013-08-28 09:23:37 +0200

    SWTBotless implementation of ObjectUtil added

--- org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/util/ObjectUtil.java
+++ org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/util/ObjectUtil.java
@@ -3,53 +3,58 @@ package org.jboss.reddeer.swt.util;
 import java.lang.reflect.Method;
 
 import org.eclipse.swt.widgets.Widget;
-import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
-import org.eclipse.swtbot.swt.finder.results.Result;
-import org.jboss.reddeer.swt.exception.SWTLayerException;
 
+/**
+ * Object util contains helper methods for invoking methods by reflections, etc.
+ * 
+ * @author Jiri Peterka
+ * 
+ */
 public class ObjectUtil {
 
 	/**
-	 * Invokes method
-	 * @param object
-	 * @param methodName
-	 * @return
+	 * Invokes method by reflection, widget based method are executed in ui thread
+	 * 
+	 * @param object given instance
+	 * @param methodName method name to be invoked
+	 * @return returns result of the method invocation
 	 */
 	public static Object invokeMethod(final Object object, String methodName) {
-			
+
 		final Method method;
 		try {
-			method = object.getClass().getMethod(methodName,
-					new Class[0]);
+			method = object.getClass().getMethod(methodName, new Class[0]);
 		} catch (Exception e) {
 			throw new RuntimeException("Cannot get method : " + e.getMessage());
 		}
-		Widget widget = null;
 		final Object result;
 		if (object instanceof Widget) {
-			widget = (Widget) object;
-			result = UIThreadRunnable.syncExec(widget.getDisplay(),
-					new Result<Object>() {
-						public Object run() {
-							Object o = null;
-							try {
-								o = method.invoke(object, new Object[0]);
-							} catch (Exception e) {
-								try {
-									throw new RuntimeException("Cannot invoke method : " + e.getMessage());
-								} catch (Exception e1) {
-									// TODO Auto-generated catch block
-									e1.printStackTrace();
-								}
-							}
-							return o;
+
+			result = Display.syncExec(new ResultRunnable<Object>() {
+
+				@Override
+				public Object run() {
+					Object o = null;
+					try {
+						o = method.invoke(object, new Object[0]);
+					} catch (Exception e) {
+						try {
+							throw new RuntimeException(
+									"Cannot invoke method : " + e.getMessage());
+						} catch (Exception e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
 						}
-					});
+					}
+					return o;
+				}
+			});
 		} else
 			try {
 				result = method.invoke(object, new Object[0]);
 			} catch (Exception e) {
-				throw new RuntimeException("Cannot invoke method : " + e.getMessage());
+				throw new RuntimeException("Cannot invoke method : "
+						+ e.getMessage());
 			}
 
 		return result;
