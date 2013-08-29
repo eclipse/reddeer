4d1d7965f175c80d1f45ecd549447c7876e72c7b
Jiri Peterka <peterka.jiri@gmail.com>
2013-08-29 15:23:33 +0200

    Added support to pause failed test (-DpauseFailedTest=true)

---
+++ org.jboss.reddeer.junit/src/org/jboss/reddeer/junit/ExecutionSetting.java
@@ -0,0 +1,55 @@
+package org.jboss.reddeer.junit;
+
+
+/**
+ * Recognized RedDeer test execution parameters 
+ * @author Jiri Peterka
+ *
+ */
+public class ExecutionSetting {
+	private static ExecutionSetting instance;
+	private boolean debugEnabled = true;
+	private boolean pauseFailedTest = false; 
+	
+	
+	/**
+	 * Provides ExecutionSetting instance
+	 * @return instance
+	 */
+	public static ExecutionSetting getInstance() {
+		if (instance == null) {
+			instance = new ExecutionSetting();
+			instance.debugEnabled = instance.readSystemProperty("logDebug",true);
+			instance.pauseFailedTest = instance.readSystemProperty("pauseFailedTest",false);
+		}
+		return instance;
+	}
+	
+	private boolean readSystemProperty(String var, boolean b) {
+		String val = System.getProperty(var);
+		if ((val == null) || (!val.equals("false") && (!val.equals("true")))) {
+			return b;
+		} else return Boolean.parseBoolean(val);		
+	}
+
+	private ExecutionSetting() {
+		
+	}
+
+	/**
+	 * isDebugEnabled getter
+	 * @return true if -DlogDebug=true (default), false otherwise
+	 */
+	public boolean isDebugEnabled() {
+		return debugEnabled;
+	}
+	
+	/**
+	 * pauseFailedTest getter
+	 * @return true if -DpauseFailingTest=true, false otherwise (default)
+	 */	
+	public boolean isPauseFailedTest() {
+		return pauseFailedTest;
+	}
+	
+}
