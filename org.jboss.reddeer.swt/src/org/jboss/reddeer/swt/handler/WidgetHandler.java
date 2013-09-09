3712c3470b74d003b972b5e358a9eb5345427ddb
Milos Prchlik <mprchlik@redhat.com>
2013-09-09 09:53:08 +0200

    Add deselect() method for tables

--- org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/handler/WidgetHandler.java
+++ org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/handler/WidgetHandler.java
@@ -400,6 +400,8 @@ public class WidgetHandler {
 			public void run() {
 				if (w instanceof List)
 					((List) w).deselectAll();
+				else if (w instanceof Table)
+					((Table) w).deselectAll();
 				else
 					throw new SWTLayerException("Unsupported type");
 			}
