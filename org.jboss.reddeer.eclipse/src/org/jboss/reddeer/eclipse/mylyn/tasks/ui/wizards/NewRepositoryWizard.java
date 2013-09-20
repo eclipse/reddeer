414b9d147d727c5b08e6c974401f14e9203241c3
Len DiMaggio <ldimaggi@redhat.com>
2013-09-20 15:00:46 -0400

    Added new files to support Red Deer task: https://github.com/jboss-reddeer/reddeer/issues/278
    Added tests for new clases - Oct 2

---
+++ org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/mylyn/tasks/ui/wizards/NewRepositoryWizard.java
@@ -0,0 +1,17 @@
+package org.jboss.reddeer.eclipse.mylyn.tasks.ui.wizards;
+
+import org.jboss.reddeer.eclipse.jface.wizard.NewWizardDialog;
+
+/**
+ * Represents new file creation wizard dialog (General -> File)
+ * 
+ * @author ldimaggi
+ *
+ */
+public class NewRepositoryWizard extends NewWizardDialog {
+
+	public NewRepositoryWizard() {
+		super("General", "File");
+	}
+
+}
