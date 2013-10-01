6a888e05f6328bb6ca47e122db6e4fc170a14caf
Jiri Peterka <peterka.jiri@gmail.com>
2013-10-01 15:12:34 +0200

    SWTBot dependencies removed from ExpandBarTest

--- org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/impl/expandbar/ExpandBarTest.java
+++ org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/impl/expandbar/ExpandBarTest.java
@@ -6,13 +6,10 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.ExpandItem;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swtbot.swt.finder.finders.UIThreadRunnable;
-import org.eclipse.swtbot.swt.finder.results.VoidResult;
 import org.jboss.reddeer.swt.api.ExpandBar;
 import org.jboss.reddeer.swt.api.ExpandBarItem;
 import org.jboss.reddeer.swt.exception.SWTLayerException;
@@ -20,6 +17,7 @@ import org.jboss.reddeer.swt.impl.expandbar.DefaultExpandBar;
 import org.jboss.reddeer.swt.impl.expandbar.DefaultExpandBarItem;
 import org.jboss.reddeer.swt.impl.text.DefaultText;
 import org.jboss.reddeer.swt.test.RedDeerTest;
+import org.jboss.reddeer.swt.util.Display;
 import org.junit.After;
 import org.junit.Test;
 
@@ -32,12 +30,11 @@ public class ExpandBarTest extends RedDeerTest{
 	@Override
 	public void setUp() {
 		super.setUp();
-		UIThreadRunnable.syncExec(new VoidResult() {
+		Display.syncExec(new Runnable() {
 
 			@Override
 			public void run() {
-				Display display = Display.getDefault();
-				Shell shell = new Shell(display);
+				Shell shell = new Shell(org.eclipse.swt.widgets.Display.getDefault());  
 				shell.setText("Testing shell");
 				createControls(shell);
 				shell.open();
@@ -82,7 +79,7 @@ public class ExpandBarTest extends RedDeerTest{
 	}
 	@After
 	public void cleanup() {
-		UIThreadRunnable.syncExec(new VoidResult() {
+		Display.syncExec(new Runnable() {
 			@Override
 			public void run() {
 				for (Shell shell : org.jboss.reddeer.swt.
