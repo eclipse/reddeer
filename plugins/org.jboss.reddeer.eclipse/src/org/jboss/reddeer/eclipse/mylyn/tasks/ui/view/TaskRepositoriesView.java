3c80afafab8404b2bbc886a9950665a655bd26b0
rawagner <rawagner@redhat.com>
2013-10-08 10:16:35 +0200

    Moved Mylyn related classes to fix the build

--- plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/mylyn/tasks/ui/view/TaskRepositoriesView.java
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/mylyn/tasks/ui/view/TaskRepositoriesView.java
@@ -3,22 +3,15 @@ package org.jboss.reddeer.eclipse.mylyn.tasks.ui.view;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.jboss.reddeer.eclipse.exception.EclipseLayerException;
 
-//import org.jboss.reddeer.eclipse.ui.ide.NewRepositoryWizard;  
 import org.jboss.reddeer.eclipse.mylyn.tasks.ui.wizards.NewRepositoryWizard;
-
-//import org.jboss.reddeer.eclipse.wst.server.ui.wizard.NewServerWizardDialog;
 import org.jboss.reddeer.swt.api.Tree;
 import org.jboss.reddeer.swt.api.TreeItem;
-//import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
 import org.jboss.reddeer.swt.exception.SWTLayerException;
 import org.jboss.reddeer.swt.impl.menu.ContextMenu;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.tree.DefaultTree;
-//import org.jboss.reddeer.swt.wait.TimePeriod;
-//import org.jboss.reddeer.swt.wait.WaitUntil;
 import org.jboss.reddeer.workbench.view.View;
 
 
@@ -31,8 +24,6 @@ import org.jboss.reddeer.workbench.view.View;
 public class TaskRepositoriesView extends View {
 	
 	public static final String TITLE = "Task Repositories";
-
-	private static final Logger log = Logger.getLogger(TaskRepositoriesView.class);
 	
 	public TaskRepositoriesView() {
 		super(TITLE);
