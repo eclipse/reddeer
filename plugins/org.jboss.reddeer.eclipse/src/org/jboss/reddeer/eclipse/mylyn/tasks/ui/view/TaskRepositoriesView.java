8c5bca2ca4f1f799bbb5f7a916305529b8c53067
Len DiMaggio <ldimaggi@redhat.com>
2013-11-22 11:40:13 -0500

    Updated per Rasta's suggestions:
    Changed TaskListView.getTask return type from DefaultTreeItem to
    TreeItem
    
    TaskList.java - Removed the stub "open" method
    
    TaskListView.java - Removed "findTask" method, renamed
    "findAndReturnTask" method to "getTask"
    
    TaskRepositoriesView.java - Removed unnecessary statements from
    "saveTask" method
    
    TaskListViewTest.java - Updated to be in synch with other changes
    
    Added TaskList and TaskListView to support automated Mylyn tests for
    local mylyn repo operations.
    
    Red Deer task = https://github.com/jboss-reddeer/reddeer/issues/397
    
    Ammened commit - added tests for new View class
    
    Ammended commit - to correct broken test: TaskListViewTest.java
    https://github.com/jboss-reddeer/reddeer/pull/397#issuecomment-29201408

--- plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/mylyn/tasks/ui/view/TaskRepositoriesView.java
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/mylyn/tasks/ui/view/TaskRepositoriesView.java
@@ -4,14 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jboss.reddeer.eclipse.exception.EclipseLayerException;
-
 import org.jboss.reddeer.eclipse.mylyn.tasks.ui.wizards.NewRepositoryWizard;
 import org.jboss.reddeer.swt.api.Tree;
 import org.jboss.reddeer.swt.api.TreeItem;
 import org.jboss.reddeer.swt.exception.SWTLayerException;
+import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.menu.ContextMenu;
+import org.jboss.reddeer.swt.impl.menu.ShellMenu;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
+import org.jboss.reddeer.swt.impl.text.DefaultText;
 import org.jboss.reddeer.swt.impl.tree.DefaultTree;
+import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
 import org.jboss.reddeer.workbench.view.View;
 
 
@@ -29,6 +32,51 @@ public class TaskRepositoriesView extends View {
 		super(TITLE);
 	}
 
+	public void saveTask () {
+		new ShellMenu("File", "Save").select(); 
+	}
+	
+	/* */
+	public void createLocalTask (List<TreeItem> repoItems, ArrayList<String> repoList) {
+		
+		int elementIndex = repoList.indexOf("Local");
+		log.info("Found Local Task Repo: " + repoItems.get(elementIndex).getText());	
+		repoItems.get(elementIndex).select();	
+		
+		new ShellMenu("File", "New", "Other...").select();  
+		new DefaultTree();
+		DefaultTreeItem theNewTask = new DefaultTreeItem ("Tasks", "Task");
+		theNewTask.select();	
+		new PushButton("Next >").click();
+		
+		/* Specify that the new task will be created in the Local repo */
+		new DefaultTree();
+		DefaultTreeItem theLocalRepo = new DefaultTreeItem ("Local");
+		theLocalRepo.select();	
+		new PushButton("Finish").click();	
+	}
+	
+	public void activateTask (String taskName) {
+		new ShellMenu("Navigate", "Activate Task...").select(); 
+		new DefaultText().setText(taskName);
+		new PushButton("OK").click();	
+	}
+	
+	public void openTask (String taskName) { 
+		new ShellMenu("Navigate", "Open Task...").select();    
+		new DefaultText().setText(taskName);
+		new PushButton("OK").click();	
+	}
+	
+	public void deactivateTask () {
+		new ShellMenu("Navigate", "Deactivate Task").select();  
+	}
+	
+	public void deleteTask () {
+		new ShellMenu("Edit", "Delete").select();  
+		new PushButton("Yes").click();	
+	}
+	
 	public NewRepositoryWizard newTaskRepositories(){
 		log.info("Creating new repository");
 		open();
