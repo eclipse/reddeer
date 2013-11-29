68e97d4a6e18f786d189ecc8d3b21640e961be4e
Milos Prchlik <mprchlik@redhat.com>
2013-11-29 13:11:12 +0100

    Allow user to specify list of jobs that should not be considered as running by JobIsRunning condition

--- plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/condition/JobIsRunning.java
+++ plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/condition/JobIsRunning.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.runtime.jobs.Job;
+import org.hamcrest.Matcher;
 
 /**
  * Returns true, if there is a non system job running, false 
@@ -13,12 +14,20 @@ import org.eclipse.core.runtime.jobs.Job;
  *
  */
 public class JobIsRunning implements WaitCondition {
+	private Matcher<String>[] ignoreJobs;
+
+	public JobIsRunning() {
+	}
+
+	public JobIsRunning(Matcher<String>... ignoreJobs) {
+		this.ignoreJobs = ignoreJobs;
+	}
 
 	@Override
 	public boolean test() {
 		return getJobs().size() != 0;
 	}
-	
+
 	@Override
 	public String description() {
 		StringBuilder msg = new StringBuilder("At least one job is running. Currently running jobs: \n");
@@ -28,13 +37,30 @@ public class JobIsRunning implements WaitCondition {
 		return msg.toString();
 	}
 	
-	private List<Job> getJobs(){
+	private List<Job> getJobs() {
 		List<Job> jobs = new ArrayList<Job>();
-		for (Job job : Job.getJobManager().find(null)){
-			if (!job.isSystem() && Job.SLEEPING != job.getState()){
-				jobs.add(job);
+
+		for (Job job: Job.getJobManager().find(null)) {
+			if (job.isSystem() || job.getState() == Job.SLEEPING)
+				continue;
+
+			if (ignoreJobs != null) {
+				boolean ignore = false;
+
+				for(Matcher<String> matcher: ignoreJobs) {
+					if (matcher.matches(job.getName())) {
+						ignore = true;
+						break;
+					}
+				}
+
+				if (ignore)
+					continue;
 			}
+
+			jobs.add(job);
 		}
+
 		return jobs;
 	}
 }
