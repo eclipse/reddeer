00aef147e579422edcd8f96e8cf84f5631bd8327
rrabara <rrabara@redhat.com>
2013-12-02 15:42:25 +0100

    Implemented Navigator View #206

---
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/ui/views/navigator/ResourceNavigator.java
@@ -0,0 +1,17 @@
+package org.jboss.reddeer.eclipse.ui.views.navigator;
+
+import org.jboss.reddeer.eclipse.jdt.ui.AbstractExplorer;
+
+/**
+ * Navigator RedDeer implementation
+ * "The Navigator view provides a hierarchical view of the resources
+ * in the Workbench including hidden files."
+ * 
+ * @author Radoslav Rabara
+ *
+ */
+public class ResourceNavigator extends AbstractExplorer {
+	public ResourceNavigator() {
+		super("Navigator");
+	}
+}
---
+++ tests/org.jboss.reddeer.eclipse.test/src/org/jboss/reddeer/eclipse/test/ui/views/navigator/ResourceNavigatorTest.java
@@ -0,0 +1,103 @@
+package org.jboss.reddeer.eclipse.test.ui.views.navigator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.jboss.reddeer.eclipse.jdt.ui.NewJavaClassWizardDialog;
+import org.jboss.reddeer.eclipse.jdt.ui.NewJavaClassWizardPage;
+import org.jboss.reddeer.eclipse.jdt.ui.ide.NewJavaProjectWizardDialog;
+import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.Project;
+import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.ProjectItem;
+import org.jboss.reddeer.eclipse.ui.views.navigator.ResourceNavigator;
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.swt.handler.WorkbenchHandler;
+import org.jboss.reddeer.swt.test.RedDeerTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Testing ResourceNavigator
+ * 
+ * @author rrabara
+ */
+@RunWith(RedDeerSuite.class)
+public class ResourceNavigatorTest extends RedDeerTest {
+	
+	private static final String PROJECT_NAME = "TestProject";
+	private static final String SOURCE_FOLDER = "src";
+	private static final String PACKAGE_NAME = "org.jboss.reddeer.eclipse.jdt.navigator";
+	private static final String CLASS_NAME = "Test";
+	
+	private ResourceNavigator navigator;
+	
+	@Override
+	public void setUp() {
+		super.setUp();
+		navigator = new ResourceNavigator();
+	}
+	
+	@Test
+	public void open() {
+		
+		navigator.open();
+		String activeViewTitle = WorkbenchHandler.getInstance().getActiveViewTitle();
+		assertTrue("Active view have to be Navigator but is"
+				+activeViewTitle, activeViewTitle.equals("Navigator"));
+	}
+	
+	/**
+	 * This test case is verifying that package
+	 * is represented as hierarchy of directories.
+	 */
+	@Test
+	public void navigation() {
+		createProject(PROJECT_NAME);
+		createClass(PROJECT_NAME, PACKAGE_NAME, CLASS_NAME);
+		
+		Project project = navigator.getProject(PROJECT_NAME);
+		ProjectItem item = project.getProjectItem(SOURCE_FOLDER);
+		for(String name : PACKAGE_NAME.split("[.]")) {
+			item = item.getChild(name);
+		}
+		item = item.getChild(CLASS_NAME+".java");
+	}
+
+	/**
+	 * Create class in specified project and package
+	 * 
+	 * @param projectName
+	 * @param packageName
+	 * @param className
+	 */
+	private void createClass(String projectName, String packageName, String className) {
+		NewJavaClassWizardDialog classDialog = new NewJavaClassWizardDialog();
+		classDialog.open();
+		
+		NewJavaClassWizardPage page = classDialog.getFirstPage();
+		page.setName(className);
+		page.setPackage(packageName);
+		page.setSourceFolder(projectName+"/"+SOURCE_FOLDER);
+		
+		classDialog.finish();
+	}
+
+	/**
+	 * Create project with given name
+	 * @param projectName
+	 */
+	private void createProject(String projectName) {
+		NewJavaProjectWizardDialog dialog = new NewJavaProjectWizardDialog();
+		dialog.open();
+		dialog.getFirstPage().setProjectName(projectName);
+		dialog.finish();
+	}
+	
+	@Override
+	protected void tearDown() {
+		if (navigator != null){
+			if(navigator.containsProject(PROJECT_NAME)) {
+				navigator.getProject(PROJECT_NAME).delete(true);
+			}
+		}
+		super.tearDown();
+	}
+}
