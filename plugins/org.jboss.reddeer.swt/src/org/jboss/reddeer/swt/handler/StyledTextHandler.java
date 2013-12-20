74936106013ede0dfe93c53b5d51600234936fd0
Jiri Peterka <peterka.jiri@gmail.com>
2013-12-20 08:27:25 +0100

    StyledText handler implemented for UI operations

---
+++ plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/handler/StyledTextHandler.java
@@ -0,0 +1,71 @@
+package org.jboss.reddeer.swt.handler;
+
+import org.eclipse.swt.custom.StyledText;
+import org.jboss.reddeer.swt.util.Display;
+
+/**
+ * StyleTextHandler provides API for manipulation with with StyledText widget
+ * @author Jiri Peterka
+ *
+ */
+public class StyledTextHandler {
+
+	private static StyledTextHandler instance;
+
+	private StyledTextHandler() {
+	}
+
+	/**
+	 * Returns StyledText handler instance
+	 * 
+	 * @return StyledText handler class instance
+	 */
+	public static StyledTextHandler getInstance() {
+		if (instance == null)
+			instance = new StyledTextHandler();
+		return instance;
+	}
+
+	/**
+	 * Set selection within StyledText
+	 * @param styledText widget instance
+	 * @param line line
+	 * @param column column
+	 */
+	public void setSelection(final StyledText styledText, final int line,
+			final int column) {
+
+		Display.syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				int offset = styledText.getContent().getOffsetAtLine(line)
+						+ column;
+				styledText.setSelection(offset);
+			}
+		});
+
+	}
+
+	/**
+	 * Insert text on current position
+	 * @param styledText widget instance
+	 * @param text 
+	 */
+	public void insertText(final StyledText styledText, final String text) {
+		Display.syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				styledText.insert(text);
+
+			}
+		});
+	}
+
+	public void insertText(StyledText styledText, int line, int column,
+			String text) {
+		setSelection(styledText, line, column);
+		insertText(styledText, text);
+	}
+}
