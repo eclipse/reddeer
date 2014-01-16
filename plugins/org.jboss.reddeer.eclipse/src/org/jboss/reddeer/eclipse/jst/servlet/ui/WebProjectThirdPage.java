c33f73c518e0a7fc4d89a0fe06f54df382608b95
rawagner <rawagner@redhat.com>
2014-01-16 16:03:13 +0100

    new Dynamic Web Project wizard added, fixes #432

---
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/jst/servlet/ui/WebProjectThirdPage.java
@@ -0,0 +1,36 @@
+package org.jboss.reddeer.eclipse.jst.servlet.ui;
+
+import org.jboss.reddeer.eclipse.jface.wizard.WizardPage;
+import org.jboss.reddeer.swt.impl.button.CheckBox;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+
+public class WebProjectThirdPage extends WizardPage{
+
+	public void setContextRoot(String contextRoot){
+		new LabeledText("Context root:").setText(contextRoot);
+	}
+	
+	public String getContextRoot(){
+		return new LabeledText("Context root:").getText();
+	}
+	
+	public void setContentDirectory(String contentDirectory){
+		new LabeledText("Content directory:").setText(contentDirectory);
+	}
+	
+	public String getContentDirectory(){
+		return new LabeledText("Content directory:").getText();
+	}
+	
+	public void setGenerateWebXmlDeploymentDescriptor(boolean generate){
+		new CheckBox("Generate web.xml deployment descriptor").toggle(generate);
+	}
+	
+	public boolean isGenerateWebXmlDeploymentDescriptor(){
+		return new CheckBox("Generate web.xml deployment descriptor").isChecked();
+	}
+	
+	
+	
+}
+
