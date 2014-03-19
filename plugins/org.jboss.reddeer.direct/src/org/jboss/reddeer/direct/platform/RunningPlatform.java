5a15180a82a331bc2811bbd29920744d7875d928
Jiri Peterka <peterka.jiri@gmail.com>
2014-03-19 10:52:35 +0100

    Reviewed org.jboss.reddeer.direct plugin javadoc (fixes #463)

--- plugins/org.jboss.reddeer.direct/src/org/jboss/reddeer/direct/platform/RunningPlatform.java
+++ plugins/org.jboss.reddeer.direct/src/org/jboss/reddeer/direct/platform/RunningPlatform.java
@@ -6,53 +6,55 @@ import org.eclipse.core.runtime.Platform;
  * Running platforms provides information about currently running instance
  * like Operation System, etc.
  * @author Jiri Peterka
- *
+ * @since 0.5
  */
 public class RunningPlatform {
 	
-	private static final String OS_NAME = Platform.getOS().toLowerCase();
+	private static final String CURRENT_OS = Platform.getOS().toLowerCase();
 
 	/**
-	 * Returns true when machine operating system is os
-	 * @param expected os 
-	 * @return returns true if given OS matches with running OS
+	 * Checks if Operating system family belongs to given OS family
+	 * @param os given os to check with  
+	 * @return returns true if given os matches with running operation system
 	 */
 	public static boolean isOperationSystem(OS os){
 		
 		boolean result = false;
 		
 		if (os.equals(OS.WINDOWS)){
-			result = OS_NAME.startsWith("win");
+			result = isWindows();
 		} 
 		else if (os.equals(OS.MACOSX)){
-			result = OS_NAME.startsWith("macosx");
+			result = isOSX();
 		}else if (os.equals(OS.LINUX)){
-			result = !(OS_NAME.startsWith("macosx") 
-					|| OS_NAME.startsWith("win"));
+			result = !(isOSX() || isWindows()); 
 		}
 		
 		return result;
 	}
 	
 	/**
+	 * Checks if current system belongs To Windows OS family 
 	 * @return true if running platform is Windows
 	 */
 	public static boolean isWindows() {
-		return OS_NAME.startsWith("win");
+		return CURRENT_OS.startsWith("win");
 	}
 
 	/**
+	 * Checks if current system belongs to Apple OSX family
 	 * @return true if running platform is OSX
 	 */	
 	public static boolean isOSX() {
-		return OS_NAME.startsWith("macosx");
+		return CURRENT_OS.startsWith("macosx");
 	}
 
 	/**
+	 * Checks if current system belongs to GNU/Linux operation system family
 	 * @return true if running platform is Linux
 	 */		
 	public static boolean isLinux() {
-		return OS_NAME.startsWith("linux");
+		return CURRENT_OS.startsWith("linux");
 	}
 
 }
