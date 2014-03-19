5b83606e9c980ae4f4fe198570a75d244aa09956
rhopp <rhopp@redhat.com>
2014-03-19 14:53:15 +0100

    ProgressBarHandler created. Fixes #401
    ProgressBarHandler was created and methods requiring sync/async
    execution in UI thread were transfered there from AbstractProgressBar
    class and its children.
    Also as part of this commit, wrong comment in TextHandler was fixed.

---
+++ plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/impl/progressbar/ProgressBarHandler.java
@@ -0,0 +1,97 @@
+package org.jboss.reddeer.swt.impl.progressbar;
+
+import org.eclipse.swt.widgets.ProgressBar;
+import org.jboss.reddeer.swt.util.Display;
+import org.jboss.reddeer.swt.util.ResultRunnable;
+
+/**
+ *	Contains methods that handle UI operations on {@link ProgressBar} widgets. 
+ * 
+ * @author rhopp
+ *
+ */
+
+
+public class ProgressBarHandler {
+	
+	private static ProgressBarHandler instance;
+	
+	
+	/**
+	 * Creates and returns instance of ProgressBarHandler class
+	 * 
+	 * @return
+	 */
+	public static ProgressBarHandler getInstance(){
+		if (instance == null){
+			instance = new ProgressBarHandler();
+		}
+		return instance;
+	}
+	
+	
+	/**
+	 * Returns state of {@link ProgressBar}
+	 * 
+	 * @param widget
+	 * @return
+	 */
+	
+	public int getState(final ProgressBar widget){
+		return Display.syncExec(new ResultRunnable<Integer>() {
+			@Override
+			public Integer run() {
+				return widget.getState();
+			}
+		});
+	}
+	
+	/**
+	 * Returns maximum of {@link ProgressBar}
+	 * 
+	 * @param widget
+	 * @return
+	 */
+	
+	public int getMaximum(final ProgressBar widget){
+		return Display.syncExec(new ResultRunnable<Integer>() {
+			@Override
+			public Integer run() {
+				return widget.getMaximum();
+			}
+		});
+	}
+	
+	/**
+	 * Returns minimum of {@link ProgressBar}
+	 * 
+	 * @param widget
+	 * @return
+	 */
+	
+	public int getMinimum(final ProgressBar widget){
+		return Display.syncExec(new ResultRunnable<Integer>() {
+			@Override
+			public Integer run() {
+				return widget.getMinimum();
+			}
+		});
+	}
+	
+	/**
+	 * Returns selection of {@link ProgressBar}
+	 * 
+	 * @param widget
+	 * @return
+	 */
+	
+	public int getSelection(final ProgressBar widget){
+		return Display.syncExec(new ResultRunnable<Integer>() {
+			@Override
+			public Integer run() {
+				return widget.getSelection();
+			}
+		});
+	}
+
+}
