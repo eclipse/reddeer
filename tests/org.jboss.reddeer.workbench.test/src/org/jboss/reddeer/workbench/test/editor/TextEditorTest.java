540041474ae1d3f5f55c899e8440c4c592a15c39
rhopp <rhopp@redhat.com>
2014-03-19 08:53:01 +0100

    Extending TextEditor capabilities. Fixes #350.

--- tests/org.jboss.reddeer.workbench.test/src/org/jboss/reddeer/workbench/test/editor/TextEditorTest.java
+++ tests/org.jboss.reddeer.workbench.test/src/org/jboss/reddeer/workbench/test/editor/TextEditorTest.java
@@ -3,48 +3,63 @@ package org.jboss.reddeer.workbench.test.editor;
 import static org.junit.Assert.assertEquals;
 
 import java.awt.AWTException;
+import java.io.IOException;
 import java.lang.reflect.Field;
-import java.util.List;
+import java.net.URL;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.swt.SWT;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.texteditor.ITextEditor;
+import org.jboss.reddeer.direct.platform.RunningPlatform;
 import org.jboss.reddeer.eclipse.jdt.ui.ProjectExplorer;
 import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.Project;
+import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.ProjectItem;
 import org.jboss.reddeer.eclipse.ui.ide.NewFileCreationWizardDialog;
 import org.jboss.reddeer.eclipse.ui.ide.NewFileCreationWizardPage;
+import org.jboss.reddeer.eclipse.ui.wizards.datatransfer.ExternalProjectImportWizardDialog;
 import org.jboss.reddeer.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
+import org.jboss.reddeer.swt.keyboard.KeyboardFactory;
 import org.jboss.reddeer.swt.test.RedDeerTest;
 import org.jboss.reddeer.swt.util.Display;
+import org.jboss.reddeer.workbench.impl.editor.DefaultEditor;
+import org.jboss.reddeer.workbench.impl.editor.TextEditor;
+import org.jboss.reddeer.workbench.exception.WorkbenchPartNotFound;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.jboss.reddeer.workbench.exception.WorkbenchPartNotFound;
-import org.jboss.reddeer.workbench.impl.editor.TextEditor;
 
 public class TextEditorTest extends RedDeerTest {
 
 	@BeforeClass
 	public static void setup(){
+		importTestProject();
 		BasicNewProjectResourceWizard projectWizard = new BasicNewProjectResourceWizard();
 		projectWizard.open();
 		projectWizard.getFirstPage().setProjectName("testProject");
 		projectWizard.finish();
 	}
-	
+
 	@AfterClass
-	public static void teardown(){
+	public static void teardownClass(){
 		ProjectExplorer pe = new ProjectExplorer();
-		List<Project> projects = pe.getProjects();
-		for (Project p: projects){
+		for (Project p : pe.getProjects()){
 			p.delete(true);
 		}
 	}
-	
+
+	@After
+	public void teardown(){
+		new DefaultEditor().close(false);;
+	}
+
 	@Test(expected=WorkbenchPartNotFound.class)
 	public void notTextEditorTest(){
-		
+
 		NewFileCreationWizardDialog newFileDialog = new NewFileCreationWizardDialog();
 		newFileDialog.open();
 		NewFileCreationWizardPage page = newFileDialog.getFirstPage();
@@ -53,7 +68,7 @@ public class TextEditorTest extends RedDeerTest {
 		newFileDialog.finish();
 		new TextEditor();
 	}
-	
+
 	@Test
 	public void getTextTest() throws AWTException{
 		NewFileCreationWizardDialog newFileDialog = new NewFileCreationWizardDialog();
@@ -65,7 +80,7 @@ public class TextEditorTest extends RedDeerTest {
 		final TextEditor editor = new TextEditor();
 		assertEquals("", editor.getText());
 		Display.syncExec(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				ITextEditor textEditor = (ITextEditor)getEditorPart(editor);
@@ -78,10 +93,93 @@ public class TextEditorTest extends RedDeerTest {
 			}
 		});
 		assertEquals("testText", editor.getText());
-		editor.save();
 	}
-	
-	
+
+	@Test
+	public void getTextAtLineTest(){
+		openJavaFile();
+		collapseTextInJavaFile();
+		assertEquals("\t\tSystem.out.println(\"\");", new TextEditor().getTextAtLine(4));
+	}
+
+	@Test
+	public void getFoldedTextTest(){
+		openJavaFile();
+		TextEditor textEditor = new TextEditor();
+		collapseTextInJavaFile();
+		assertEquals(94, textEditor.getText().replaceAll("\r", "").length());
+	}
+
+	@Test
+	public void insertTextTest(){
+		openJavaFile();
+		TextEditor textEditor = new TextEditor();
+		textEditor.insertText(4, 22, "test");
+		assertEquals("\t\tSystem.out.println(\"test\");", new TextEditor().getTextAtLine(4));
+	}
+
+	@Test
+	public void insertLineTest(){
+		openJavaFile();
+		TextEditor textEditor = new TextEditor();
+		textEditor.insertLine(4, "\t\ttestLine;");
+		assertEquals(106, textEditor.getText().replaceAll("\r", "").length()); //dirty hack for windows carriage return
+		assertEquals("\t\ttestLine;", new TextEditor().getTextAtLine(4));
+		assertEquals("\t\tSystem.out.println(\"\");", new TextEditor().getTextAtLine(5));
+	}
+
+	@Test
+	public void getSelectedTextTest(){
+		openJavaFile();
+		TextEditor textEditor = new TextEditor();
+		KeyboardFactory.getKeyboard().select(15, false);
+		assertEquals("package test;\np", textEditor.getSelectedText().replaceAll("\r", ""));
+	}
+
+	@Test
+	public void selectLineTest(){
+		openJavaFile();
+		collapseTextInJavaFile();
+		TextEditor textEditor = new TextEditor();
+		textEditor.selectLine(7);
+		assertEquals("}\n", textEditor.getSelectedText().replaceAll("\r",""));
+	}
+
+	private void collapseTextInJavaFile(){
+		moveCursorDown(4);
+		if (RunningPlatform.isOSX()){
+			KeyboardFactory.getKeyboard().invokeKeyCombination(SWT.COMMAND, SWT.KEYPAD_SUBTRACT);
+		}else{
+			KeyboardFactory.getKeyboard().invokeKeyCombination(SWT.CONTROL, SWT.KEYPAD_SUBTRACT);
+		}
+	}
+
+	private void openJavaFile(){
+		ProjectItem javaFile = new ProjectExplorer().getProject("TextEditorTestProject").getProjectItem("src", "test", "JavaClass.java");
+		javaFile.open();
+	}
+
+	private void moveCursorDown(int count){
+		for(int i=0; i<count; i++){
+			KeyboardFactory.getKeyboard().type(SWT.ARROW_DOWN);
+		}
+	}
+
+	private static void importTestProject(){
+		URL resourceURL = null; 
+		try {
+			resourceURL = FileLocator.toFileURL(Platform.getBundle("org.jboss.reddeer.workbench.test").getEntry("/"));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		ExternalProjectImportWizardDialog epw = new ExternalProjectImportWizardDialog();
+		epw.open();
+		epw.getFirstPage().setRootDirectory(resourceURL.getFile()+"resources/TextEditorTestProject/");
+		epw.getFirstPage().copyProjectsIntoWorkspace(true);
+		epw.finish();
+	}
+
 	private IEditorPart getEditorPart(TextEditor editor){
 		Field editorField = null;
 		try {
@@ -96,5 +194,5 @@ public class TextEditorTest extends RedDeerTest {
 		}
 		return null;
 	}
-	
+
 }
