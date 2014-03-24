d6d376bfd6d3a79c750bfe4feb39d4f9cc454fb7
Andrej Podhradsky <apodhrad@redhat.com>
2014-03-24 13:33:10 +0100

    Fixed JavaDoc

--- plugins/org.jboss.reddeer.junit/src/org/jboss/reddeer/junit/extensionpoint/IAfterTest.java
+++ plugins/org.jboss.reddeer.junit/src/org/jboss/reddeer/junit/extensionpoint/IAfterTest.java
@@ -2,13 +2,14 @@ package org.jboss.reddeer.junit.extensionpoint;
 
 public interface IAfterTest {
 	/**
-	 * Contains action proceeded prior test is run
-	 * Method is run only when method hasToRun() returns true
+	 * Contains action proceeded after each method. Method is run only when
+	 * method hasToRun() returns true
 	 */
 	public void runAfterTest();
+
 	/**
-	 * Returns true when method runAfterTest() has to run
-	 * Could be use to run method runAfterTest() only once
+	 * Returns true when method runAfterTest() has to run.
+	 * 
 	 * @return
 	 */
 	public boolean hasToRun();
