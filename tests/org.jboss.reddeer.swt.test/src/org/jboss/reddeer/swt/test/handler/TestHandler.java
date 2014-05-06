273d7064bac6e1922cc7b54b401b152cfe7af54e
Jiri Peterka <peterka.jiri@gmail.com>
2014-05-06 14:39:07 +0200

    Duplicate SWT test editor handler removed and refactoring performed (fixes #622)

--- tests/org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/handler/TestHandler.java
+++ tests/org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/handler/TestHandler.java
@@ -3,13 +3,26 @@ package org.jboss.reddeer.swt.test.handler;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.jboss.tools.reddeer.swt.test.model.TestModel;
 
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
 public class TestHandler extends AbstractHandler {
-
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		return null;
+	/**
+	 * The constructor.
+	 */
+	public TestHandler() {
 	}
 
-
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		TestModel.setClicked();
+		return null;
+	}
 }
