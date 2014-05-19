5404105a174308c837008d1ddcfe193386372b08
rawagner <rawagner@redhat.com>
2014-05-19 16:35:32 +0200

    Added ability to select text in styled text. #645

--- plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/handler/StyledTextHandler.java
+++ plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/handler/StyledTextHandler.java
@@ -1,6 +1,7 @@
 package org.jboss.reddeer.swt.handler;
 
 import org.eclipse.swt.custom.StyledText;
+import org.jboss.reddeer.swt.exception.SWTLayerException;
 import org.jboss.reddeer.swt.util.Display;
 import org.jboss.reddeer.swt.util.ResultRunnable;
 
@@ -64,6 +65,13 @@ public class StyledTextHandler {
 		});
 	}
 
+	/**
+	 * Insert text on specified position
+	 * @param styledText widget instance
+	 * @param line where to insert text
+	 * @param column where to insert text 
+	 * @param text to inser
+	 */
 	public void insertText(StyledText styledText, int line, int column,
 			String text) {
 		setSelection(styledText, line, column);
@@ -76,13 +84,62 @@ public class StyledTextHandler {
 	 * @param text
 	 * @return -1 when text is not found, position otherwise
 	 */
-	
 	public int getPositionOfText(final StyledText styledText, final String text) {
 		return Display.syncExec(new ResultRunnable<Integer>() {
 
 			@Override
 			public Integer run() {
 				return styledText.getText().indexOf(text);
+			}
+		});
+	}
+	
+	/**
+	 * Finds first occurrence of specified text and selects it
+	 * @param styledText widget instance
+	 * @param text to select
+	 */
+	public void selectText(final StyledText styledText, final String text) {
+		final int position = getPositionOfText(styledText, text);
+		if(position == -1){
+			throw new SWTLayerException("Unable to find text "+text+" in styled text");
+		}
+		Display.syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				styledText.setSelection(position, position+text.length());
+			}
+		});
+	}
+
+	/**
+	 * Selects specified position (cursor will be moved there)
+	 * @param styledText widget instance
+	 * @param position to select
+	 */
+	public void selectPosition(final StyledText styledText, final int position) {
+		Display.syncExec(new Runnable() {
+
+			@Override
+			public void run() {
+				styledText.setSelection(position);
+			}
+		});
+		
+	}
+
+	/**
+	 * Returns selected text
+	 * @param styledText widget instance
+	 * @return selected text
+	 */
+	public String getSelectionText(final StyledText styledText) {
+		return Display.syncExec(new ResultRunnable<String>() {
+
+			@Override
+			public String run() {
+				return styledText.getSelectionText();
 			}
 		});
 	}
