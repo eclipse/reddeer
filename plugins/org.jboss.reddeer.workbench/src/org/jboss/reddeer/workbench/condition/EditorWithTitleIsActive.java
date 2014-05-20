56f44aed6868f7696ffd6b2b3e5f6e47baf35a24
rawagner <rawagner@redhat.com>
2014-05-20 08:57:34 +0200

    Editor with title is active condition added. #648

---
+++ plugins/org.jboss.reddeer.workbench/src/org/jboss/reddeer/workbench/condition/EditorWithTitleIsActive.java
@@ -0,0 +1,33 @@
+package org.jboss.reddeer.workbench.condition;
+
+import org.jboss.reddeer.swt.condition.WaitCondition;
+import org.jboss.reddeer.workbench.impl.editor.DefaultEditor;
+
+/**
+ * Check whether editor is active
+ * @author rawagner
+ *
+ */
+public class EditorWithTitleIsActive implements WaitCondition{
+	
+	private String title;
+	
+	/**
+	 * 
+	 * @param title of editor
+	 */
+	public EditorWithTitleIsActive(String title){
+		this.title = title;
+	}
+
+	@Override
+	public boolean test() {
+		return new DefaultEditor().getTitle().equals(title);
+	}
+
+	@Override
+	public String description() {
+		return "Editor title is "+title;
+	}
+
+}
