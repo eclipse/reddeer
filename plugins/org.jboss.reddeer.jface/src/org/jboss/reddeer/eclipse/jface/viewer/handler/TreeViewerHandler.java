e44eb338b007045253eb187a164743bcfd82e774
Marián Labuda <mlabuda@redhat.com>
2014-06-18 13:37:38 +0200

    Styled tree item refactoring - moved to jface plugin (fixes #687)

--- plugins/org.jboss.reddeer.jface/src/org/jboss/reddeer/eclipse/jface/viewer/handler/TreeViewerHandler.java
+++ plugins/org.jboss.reddeer.jface/src/org/jboss/reddeer/eclipse/jface/viewer/handler/TreeViewerHandler.java
@@ -2,8 +2,12 @@ package org.jboss.reddeer.eclipse.jface.viewer.handler;
 
 import java.util.Iterator;
 
+import org.eclipse.swt.custom.StyleRange;
 import org.jboss.reddeer.eclipse.jface.exception.JFaceLayerException;
 import org.jboss.reddeer.swt.api.TreeItem;
+import org.jboss.reddeer.swt.exception.SWTLayerException;
+import org.jboss.reddeer.swt.util.Display;
+import org.jboss.reddeer.swt.util.ResultRunnable;
 import org.jboss.reddeer.junit.logging.Logger;
 
 public class TreeViewerHandler {
@@ -38,7 +42,7 @@ public class TreeViewerHandler {
 		Iterator<TreeItem> iterator = treeItem.getItems().iterator();
 		while (iterator.hasNext()) {
 			TreeItem item = iterator.next();
-			if (item.getNonStyledText().equals(name)) {
+			if (getNonStyledText(item).equals(name)) {
 				logger.info("Tree item with non styled text " + name +
 						" has been found");
 				return item;
@@ -46,7 +50,7 @@ public class TreeViewerHandler {
 		}
 		
 		throw new JFaceLayerException("Tree item with text "
-				+ treeItem.getNonStyledText() + " (without decorators)"
+				+ getNonStyledText(treeItem) + " (without decorators)"
 				+ " has no tree item with text " + name + " (without decorators).");
 	}
 	
@@ -70,5 +74,117 @@ public class TreeViewerHandler {
 		}
 		
 		return getTreeItem(item, path[index]);
+	}
+	
+	private TreeItemTexts parseText(TreeItem item) {
+		final org.eclipse.swt.widgets.TreeItem swtTreeItem = item.getSWTWidget();
+		return Display.syncExec(new ResultRunnable<TreeViewerHandler.TreeItemTexts>() {
+
+			@Override
+			public TreeViewerHandler.TreeItemTexts run() {
+				String nonStyledText = null;
+				String[] styledTexts = null;
+				StyleRange[] styleRanges = getStyleRanges(swtTreeItem);
+				
+				if (styleRanges == null) {
+					// Everything is ok, there is no styled texts
+					nonStyledText = swtTreeItem.getText().trim();
+					styledTexts = null;
+				} else {
+					// Here it goes. There are some styled texts
+					String rawText = swtTreeItem.getText();
+					String[] tmpStyledTexts = new String[styleRanges.length];
+					String tmpNonStyledText = null;
+					int currentTextIndex = 0;
+					int i = 0;
+					
+					for (StyleRange range: getStyleRanges(swtTreeItem)) {
+						// At some point there is a non-styled text
+						if (range.start > currentTextIndex) {
+							tmpNonStyledText = rawText.substring(currentTextIndex, 
+									range.start).trim();
+						}
+						
+						tmpStyledTexts[i] = rawText.substring(range.start, range.start +
+								range.length).trim();
+						currentTextIndex = range.start + range.length + 1;
+						i++;
+					}
+					
+					if (tmpNonStyledText == null) {
+						tmpNonStyledText = rawText.substring(currentTextIndex).trim();
+					}
+					
+					nonStyledText = tmpNonStyledText;
+					styledTexts = tmpStyledTexts;
+				}
+				
+				return new TreeItemTexts(nonStyledText, styledTexts);
+			}
+		});
+	}
+
+	// Should be run inside Display.syncExec method - in parseText method
+	// it is also expected, that styled in array are sequential
+	private StyleRange[] getStyleRanges(org.eclipse.swt.widgets.TreeItem swtTreeItem) {
+		Object data = swtTreeItem.getData("org.eclipse.jfacestyled_label_key_0");
+
+		if (data == null) {
+			return null;
+		}
+
+		if (!(data instanceof StyleRange[])) {
+			throw new JFaceLayerException(
+					"Cannot parse tree item label. Data for key "
+					+ "'org.eclipse.jfacestyled_label_key_0' are "
+					+ "expected to be of type "	+ StyleRange[].class
+					+ " but are " + data.getClass());
+		}
+
+		StyleRange[] styles = (StyleRange[]) data;
+		
+		if (styles.length == 0) {
+			return null;
+		}
+
+		return styles;
+	}
+
+
+	/**
+	 * Gets non-styled text of the tree item.
+	 *
+	 * @return non-styled text (without decorators) of the tree item
+	 */
+	public String getNonStyledText(TreeItem item) {
+		return parseText(item).getNonStyledText();
+	}
+	
+	/**
+	 * Gets styled texts on the tree item. There could be more than 1
+	 * styled texts.
+	 *
+	 * @return styled texts of the tree item or null if there are not styled texts
+	 */
+	public String[] getStyledTexts(TreeItem item) {
+		return parseText(item).getStyledTexts();
+	}
+	
+	class TreeItemTexts {
+		private String nonStyledText;
+		private String[] styledTexts;
+		
+		public TreeItemTexts(String nonStyledText, String[] styledTexts) {
+			this.nonStyledText = nonStyledText;
+			this.styledTexts = styledTexts;
+		}
+		
+		public String getNonStyledText() {
+			return nonStyledText;
+		}
+		
+		public String[] getStyledTexts() {
+			return styledTexts;
+		}
 	}
 }
