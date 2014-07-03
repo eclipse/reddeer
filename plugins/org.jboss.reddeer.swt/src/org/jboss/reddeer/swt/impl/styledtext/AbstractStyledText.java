9301ae9f1839b17d399990ace943c93077225a10
rawagner <rawagner@redhat.com>
2014-07-03 16:11:02 +0200

    AbstractStyledText created. Fixes #649

---
+++ plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/impl/styledtext/AbstractStyledText.java
@@ -0,0 +1,122 @@
+package org.jboss.reddeer.swt.impl.styledtext;
+
+import org.jboss.reddeer.common.logging.Logger;
+import org.jboss.reddeer.swt.api.StyledText;
+import org.jboss.reddeer.swt.handler.StyledTextHandler;
+import org.jboss.reddeer.swt.handler.WidgetHandler;
+
+/**
+ * Abstract Styled Text contains common rutines for styled text widget.
+ * Concrete impementations of styledtext should extend this class.
+ * @author rawagner
+ */
+public abstract class AbstractStyledText implements StyledText {
+    
+    /**
+     * Styled text SWT widget instance.
+     */
+    protected org.eclipse.swt.custom.StyledText styledText;
+    
+    /**
+     * Logger.
+     */
+    protected final Logger log = Logger.getLogger(this.getClass());
+
+    /**
+     * Returns styledtext text.
+     * @return text of this StyledText
+     */
+    @Override
+    public String getText() {
+        String text = WidgetHandler.getInstance().getText(styledText);
+        return text;
+    }
+
+    /**
+     * Sets text of this StyledText.
+     * @param text to set
+     */
+    @Override
+    public void setText(final String text) {
+        log.info("Text set to: " + text);
+        WidgetHandler.getInstance().setText(styledText, text);
+    }
+
+    /**
+     * Returns tooltip text.
+     * @return Tooltip text of this StyledText
+     */
+    @Override
+    public String getToolTipText() {
+        String tooltipText = WidgetHandler.getInstance().getToolTipText(styledText);
+        return tooltipText;
+    }
+
+    /**
+     * Insert text into styled text content.
+     * @param line line to insert text
+     * @param column column to insert text
+     * @Param text to insert
+     */
+    @Override
+    public void insertText(final int line, final int column, final String text) {
+        StyledTextHandler.getInstance().insertText(styledText, line, column, text);
+    }
+
+    /**
+     * @see org.jboss.reddeer.swt.api.StyledText#insertText(String)
+     */
+    @Override
+    public void insertText(final String text) {
+        StyledTextHandler.getInstance().insertText(styledText, text);
+    }
+
+    /**
+     * @see org.jboss.reddeer.swt.widgets.Widget#getSWTWidget()
+     */
+    @Override
+    public org.eclipse.swt.custom.StyledText getSWTWidget() {
+        return styledText;
+    }
+
+    /**
+     * @see org.jboss.reddeer.swt.widgets.Widget#isEnabled()
+     */
+    @Override
+    public boolean isEnabled() {
+        return WidgetHandler.getInstance().isEnabled(styledText);
+    }
+
+    /**
+     * @see org.jboss.reddeer.swt.api.StyledText#getPositionOfText(String)
+     */
+    @Override
+    public int getPositionOfText(final String text) {
+        return StyledTextHandler.getInstance().getPositionOfText(styledText,text);
+    }
+
+    /**
+     * @see org.jboss.reddeer.swt.api.StyledText#selectText(String)
+     */
+    @Override
+    public void selectText(final String text) {
+        StyledTextHandler.getInstance().selectText(styledText, text);
+    }
+
+    /**
+     * @see org.jboss.reddeer.swt.api.StyledText#selectPosition(int)
+     */
+    @Override
+    public void selectPosition(final int position) {
+        StyledTextHandler.getInstance().selectPosition(styledText, position);
+    }
+
+    /**
+     * @see org.jboss.reddeer.swt.api.StyledText#getSelectionText()
+     */
+    @Override
+    public String getSelectionText() {
+        return StyledTextHandler.getInstance().getSelectionText(styledText);
+    }
+
+}
