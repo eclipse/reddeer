5c3a68fa81a46bd1f616e0bfb473e7dd92170d4f
rhopp <rhopp@redhat.com>
2014-08-13 10:45:45 +0200

    ToolBarMenu renamed to ViewMenu (fixes #748)

--- plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/impl/menu/ViewMenu.java
+++ plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/impl/menu/ViewMenu.java
@@ -4,38 +4,51 @@ import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.swt.widgets.MenuItem;
 import org.hamcrest.Matcher;
 import org.jboss.reddeer.common.logging.Logger;
-import org.jboss.reddeer.swt.api.Menu;
 import org.jboss.reddeer.swt.handler.ActionContributionItemHandler;
 import org.jboss.reddeer.swt.matcher.WithMnemonicTextMatchers;
 
 /**
- * ToolbarMenu implementation
- * @author Rastislav Wagner
+ * ViewMenu implementation.
  * 
- * @deprecated since 0.6. Use {@link ViewMenu} instead.
+ * @author rhopp
  *
  */
-public class ToolbarMenu extends AbstractMenu implements Menu{
-	
-	private static final Logger log = Logger.getLogger(ToolbarMenu.class);
-	
+
+public class ViewMenu extends AbstractMenu {
+
+	private static final Logger LOGGER = Logger.getLogger(ToolbarMenu.class);
+
 	private ActionContributionItem item;
-	
-	public ToolbarMenu(String... path){
+
+	/**
+	 * Constructor for ViewMenu with given path as array of strings.
+	 * 
+	 * @param path
+	 *            path to desired menu item.
+	 */
+
+	public ViewMenu(String... path) {
 		this(new WithMnemonicTextMatchers(path).getMatchers());
 	}
-	
-	public ToolbarMenu(Matcher<String>... matchers){
+
+	/**
+	 * Constructor for ViewMenu with given path as array of string matchers.
+	 * 
+	 * @param matchers
+	 *            path to desired menu item.
+	 */
+
+	public ViewMenu(Matcher<String>... matchers) {
 		item = ml.lookFor(ml.getToolbarMenus(), matchers);
 		this.matchers = matchers;
 	}
 
 	@Override
 	public void select() {
-		log.info("Select toolbar menu with text " + getText());
+		LOGGER.info("Select toolbar menu with text " + getText());
 		mh.select(item);
 	}
-	
+
 	@Override
 	public boolean isSelected() {
 		return false;
@@ -45,15 +58,20 @@ public class ToolbarMenu extends AbstractMenu implements Menu{
 	public String getText() {
 		return item.getAction().getText().replace("&", "");
 	}
-	
-	public MenuItem getSWTWidget(){
+
+	/**
+	 * Returns SWT widget representing this menu item.
+	 * 
+	 * @return SWT widget.
+	 */
+
+	public MenuItem getSWTWidget() {
 		return null;
 	}
-	
+
 	@Override
 	public boolean isEnabled() {
 		return ActionContributionItemHandler.getInstance().isEnabled(item);
 	}
 
-	
 }
--- tests/org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/impl/menu/ViewBarMenuTest.java
+++ tests/org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/impl/menu/ViewBarMenuTest.java
@@ -5,7 +5,7 @@ import org.jboss.reddeer.swt.condition.ProgressInformationShellIsActive;
 import org.jboss.reddeer.swt.condition.ShellWithTextIsAvailable;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.menu.ShellMenu;
-import org.jboss.reddeer.swt.impl.menu.ToolbarMenu;
+import org.jboss.reddeer.swt.impl.menu.ViewMenu;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
 import org.jboss.reddeer.swt.wait.WaitWhile;
@@ -13,14 +13,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(RedDeerSuite.class)
-public class ToolBarMenuTest {
+public class ViewBarMenuTest {
 	
 	@Test
 	public void testErrorLogMenu(){
 		new ShellMenu("Window","Show View","Other...").select();
 		new DefaultTreeItem("General","Error Log").select();
 		new PushButton("OK").click();
-		new ToolbarMenu("View Menu","Filters...").select();
+		new ViewMenu("View Menu","Filters...").select();
 		new DefaultShell("Log Filters");
 		new PushButton("OK").click();
 		new WaitWhile(new ShellWithTextIsAvailable("Log Filters"));
