2b36d2084b0144a1905f684b8666596e5d96f3f3
rrabara <rrabara@redhat.com>
2014-08-21 13:19:41 +0200

    Improved Preference Dialog and Pages API. Fixes #782

--- plugins/org.jboss.reddeer.workbench/src/org/jboss/reddeer/workbench/preference/WorkbenchPreferencePage.java
+++ plugins/org.jboss.reddeer.workbench/src/org/jboss/reddeer/workbench/preference/WorkbenchPreferencePage.java
@@ -17,10 +17,14 @@ import org.jboss.reddeer.swt.util.ResultRunnable;
  * should represent the concrete preference page.
  * 
  * @author Lucia Jelinkova
+ * @author Radoslav Rabara
  * @since 0.6 
  */
 public class WorkbenchPreferencePage extends PreferencePage {
 
+	/**
+	 * @deprecated since 0.6, {@link WorkbenchPreferencePage#DIALOG_TITLE} should be used
+	 */
 	public static final String DIALOG_TITLE = "Preferences";
 
 	protected final Logger log = Logger.getLogger(this.getClass());
@@ -28,12 +32,25 @@ public class WorkbenchPreferencePage extends PreferencePage {
 	private String[] path;
 
 	/**
-	 * Constructor sets path to specific preference item. 
+	 * Constructor sets path to specific preference item.
 	 * @param path path in preference shell tree to specific preference
 	 */
 	public WorkbenchPreferencePage(String... path) {
 		super();
+		if (path == null) {
+			throw new IllegalArgumentException("path can't be null");
+		}
+		if (path.length == 0) {
+			throw new IllegalArgumentException("path can't be empty");
+		}
 		this.path = path;
+	}
+
+	/**
+	 * Returns path of the preference item.
+	 */
+	public String[] getPath() {
+		return path.clone();
 	}
 
 	/**
