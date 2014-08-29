34ac16e348576e124b0ee40c81cf24f6ca6eb6fe
rrabara <rrabara@redhat.com>
2014-08-29 09:55:58 +0200

    Implemented method remove() in ServerModule. Fixes #799

--- plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/wst/server/ui/view/ServerModule.java
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/wst/server/ui/view/ServerModule.java
@@ -1,6 +1,14 @@
 package org.jboss.reddeer.eclipse.wst.server.ui.view;
 
+import org.jboss.reddeer.eclipse.exception.EclipseLayerException;
 import org.jboss.reddeer.swt.api.TreeItem;
+import org.jboss.reddeer.swt.condition.JobIsRunning;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsActive;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsAvailable;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.menu.ShellMenu;
+import org.jboss.reddeer.swt.wait.WaitUntil;
+import org.jboss.reddeer.swt.wait.WaitWhile;
 
 /**
  * Represents a module assigned to server {@link Server} on {@link ServersView}.
@@ -9,21 +17,36 @@ import org.jboss.reddeer.swt.api.TreeItem;
  *
  */
 public class ServerModule {
-	
+
 	protected TreeItem treeItem;
-	
+
 	public ServerModule(TreeItem item) {
+		if (item == null) {
+			throw new IllegalArgumentException("item can't be null");
+		}
 		this.treeItem = item;
 	}
-	
-	public ModuleLabel getLabel(){
+
+	/**
+	 * Returns module's label as {@link ModuleLabel}.
+	 */
+	public ModuleLabel getLabel() {
 		return new ModuleLabel(treeItem);
 	}
-	
+
 	/**
-	 * Removes the server module. Not yet implemented!
+	 * Removes the server module.
 	 */
 	public void remove(){
-		throw new UnsupportedOperationException();
+		if (treeItem == null) {
+			throw new EclipseLayerException("ServerModule was already removed");
+		}
+		treeItem.select();
+		new ShellMenu("Edit", "Delete").select();
+		new WaitUntil(new ShellWithTextIsActive("Server"));
+		new PushButton("OK").click();
+		new WaitWhile(new ShellWithTextIsAvailable("Server"));
+		new WaitWhile(new JobIsRunning());
+		treeItem = null;
 	}
 }
