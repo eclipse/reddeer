6913ab4f5a66bc5cd333d8f43ceb4e69f9a9e362
vpakan <vpakan@redhat.com>
2014-10-02 13:56:48 +0200

    Adds possibity to manage view containig project from within project itself Fixes: #839

--- plugins/org.jboss.reddeer.workbench/src/org/jboss/reddeer/workbench/handler/WorkbenchPartHandler.java
+++ plugins/org.jboss.reddeer.workbench/src/org/jboss/reddeer/workbench/handler/WorkbenchPartHandler.java
@@ -1,11 +1,21 @@
 package org.jboss.reddeer.workbench.handler;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionFactory;
 import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+import org.jboss.reddeer.common.logging.Logger;
+import org.jboss.reddeer.swt.handler.WidgetHandler;
 import org.jboss.reddeer.swt.util.Display;
 import org.jboss.reddeer.swt.util.ResultRunnable;
+import org.jboss.reddeer.swt.widgets.Widget;
+import org.jboss.reddeer.workbench.exception.WorkbenchLayerException;
 
 /**
  * WorkbenchPart handler handles operations which are common for both editor and view instances
@@ -15,6 +25,8 @@ import org.jboss.reddeer.swt.util.ResultRunnable;
 public class WorkbenchPartHandler {
 	
 	private static WorkbenchPartHandler instance;
+	
+	protected final Logger log = Logger.getLogger(this.getClass());
 	
 	private WorkbenchPartHandler(){
 		
@@ -76,7 +88,118 @@ public class WorkbenchPartHandler {
 			}
 		});
 	}
+	/**
+	 * Activates Workbench part containing specified widget
+	 * @param widget
+	 */
+	public void activateWorkbenchPartWithWidget (Widget widget) {
+		
+		final WorkbenchPartWidgets workbenchPartWidgets = getWorkbenchPartWidgetsForWidget(widget);
+		
+		if (workbenchPartWidgets != null){
+			Display.syncExec(new Runnable() {			
+				@Override
+				public void run() {
+					CTabItem[] cTabItems = workbenchPartWidgets.workbenchPartCTabFolder.getItems();
+					int index = 0;
+					boolean cTabItemFound = false;
+					while (!cTabItemFound && index < cTabItems.length){
+						if (cTabItems[index].getControl() == workbenchPartWidgets.firstCTabItemControl){
+							cTabItemFound = true;
+							if (workbenchPartWidgets.workbenchPartCTabFolder.getSelectionIndex() != index){
+								log.debug("Activating Workbench part with label: " + cTabItems[index].getText());
+								workbenchPartWidgets.workbenchPartCTabFolder.setSelection(index);	
+							}
+						}		
+						else{
+							index++;
+						}
+					}
+				}
+			});
+		}
+		else{
+			throw new WorkbenchLayerException("Unable to activate workbench part with widget. No cTabFolder found in widget path");
+		}
+	}
 	
+	/**
+	 * Gets title of Workbench part containing specified widget
+	 * @param widget
+	 */
+	public String getTitleOfWorkbenchPartWithWidget (Widget widget) {
+		
+		final WorkbenchPartWidgets workbenchPartWidgets = getWorkbenchPartWidgetsForWidget(widget);
+		
+		if (workbenchPartWidgets != null){
+			return Display.syncExec(new ResultRunnable<String>() {
+				@Override
+				public String run() {
+					CTabItem[] cTabItems = workbenchPartWidgets.workbenchPartCTabFolder.getItems();
+					int index = 0;
+					String cTabItemTitle = null;
+					while (cTabItemTitle == null && index < cTabItems.length){
+						if (cTabItems[index].getControl() == workbenchPartWidgets.firstCTabItemControl){
+							cTabItemTitle = cTabItems[index].getText();
+						}		
+						else{
+							index++;
+						}
+					}
+					return cTabItemTitle;
+				}
+			});
+		}
+		else{
+			throw new WorkbenchLayerException("Unable to get title of workbench part with widget. No cTabFolder found in widget path");
+		}
+	}
 	
+	/**
+	 * Stores information about top workbench part widgets
+	 * @author vlado pakan
+	 *
+	 */
+	private class WorkbenchPartWidgets {
+		
+		public WorkbenchPartWidgets(CTabFolder workbenchPartCTabFolder,
+				Control firstCTabItemControl) {
+			super();
+			this.workbenchPartCTabFolder = workbenchPartCTabFolder;
+			this.firstCTabItemControl = firstCTabItemControl;
+		}
+		
+		public CTabFolder workbenchPartCTabFolder = null;
+		public Control firstCTabItemControl = null;
+
+	}
+	
+	/**
+	 * Returns CTabFolder and CTabItem containing specified widget
+	 * @param widget - widget contained within returned {@link WorkbenchPartWidgets}
+	 * @return {@link WorkbenchPartWidgets}
+	 */
+	private WorkbenchPartWidgets getWorkbenchPartWidgetsForWidget (Widget widget){
+		
+		WorkbenchPartWidgets workbenchPartWidgets = null;
+		
+		List<org.eclipse.swt.widgets.Widget> pathToWidget = WidgetHandler.getInstance().getPathToWidget(widget.getSWTWidget());
+		Iterator<org.eclipse.swt.widgets.Widget> itWidget = pathToWidget.iterator();
+		boolean foundCTabFolder = false;
+		org.eclipse.swt.widgets.Widget currentWidget = null;
+		while (!foundCTabFolder && itWidget.hasNext()){
+			currentWidget = itWidget.next();
+			if (currentWidget instanceof org.eclipse.swt.custom.CTabFolder){
+				foundCTabFolder = true;
+			}
+		}
+		
+		if (foundCTabFolder){
+			workbenchPartWidgets = new WorkbenchPartWidgets((CTabFolder)currentWidget,
+				(Control)itWidget.next());
+		}
+		
+		return workbenchPartWidgets;
+	}
 
 }
