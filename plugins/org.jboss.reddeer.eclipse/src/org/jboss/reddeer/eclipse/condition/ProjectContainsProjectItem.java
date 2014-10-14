196b86d5f5ab01f9a8416840aa90caef59b60f02
rawagner <rawagner@redhat.com>
2014-10-14 12:10:32 +0200

    Added condition to check if project contains project item. #844

---
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/condition/ProjectContainsProjectItem.java
@@ -0,0 +1,42 @@
+package org.jboss.reddeer.eclipse.condition;
+
+import org.jboss.reddeer.eclipse.exception.EclipseLayerException;
+import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.Project;
+import org.jboss.reddeer.swt.condition.WaitCondition;
+
+/**
+ * Returns true if project contains specified project item
+ * @author rawagner
+ *
+ */
+public class ProjectContainsProjectItem implements WaitCondition {
+	
+	private Project project;
+	private String[] path;
+	
+	/**
+	 * Default Constructor
+	 * @param project to check
+	 * @param itemPath path of item (including item) to search for
+	 */
+	public ProjectContainsProjectItem(Project project, String... itemPath) {
+		this.project = project;
+		this.path = itemPath;
+	}
+
+	@Override
+	public boolean test() {
+		try{
+			project.getProjectItem(path);
+		} catch (EclipseLayerException ex){
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String description() {
+		return "Project "+project.getName()+ " contains project item "+path[path.length-1];
+	}
+	
+}
