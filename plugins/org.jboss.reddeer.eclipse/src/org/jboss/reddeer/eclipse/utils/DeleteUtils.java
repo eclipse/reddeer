eab5547e97d56c1718619b129024463f39b3224d
rawagner <rawagner@redhat.com>
2014-11-24 12:50:09 +0100

    Check parent during deletion #884

--- plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/utils/DeleteUtils.java
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/utils/DeleteUtils.java
@@ -9,10 +9,10 @@ import org.jboss.reddeer.common.logging.Logger;
 import org.jboss.reddeer.eclipse.exception.EclipseLayerException;
 import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.Project;
 import org.jboss.reddeer.swt.condition.JobIsRunning;
+import org.jboss.reddeer.swt.condition.ShellHasChildrenOrIsNotAvailable;
 import org.jboss.reddeer.swt.condition.ShellWithTextIsAvailable;
-import org.jboss.reddeer.swt.condition.WaitCondition;
 import org.jboss.reddeer.swt.exception.SWTLayerException;
-import org.jboss.reddeer.swt.exception.WaitTimeoutExpiredException;
+import org.jboss.reddeer.swt.handler.ShellHandler;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.shell.AbstractShell;
 import org.jboss.reddeer.swt.lookup.ShellLookup;
@@ -22,116 +22,72 @@ import org.jboss.reddeer.swt.reference.ReferencedComposite;
 import org.jboss.reddeer.swt.wait.TimePeriod;
 import org.jboss.reddeer.swt.wait.WaitUntil;
 import org.jboss.reddeer.swt.wait.WaitWhile;
- 	
+
 public class DeleteUtils {
 	private static final Logger log = Logger.getLogger(DeleteUtils.class);
+
 	/**
 	 * Handles specific cases during project deletion
-	 * @param deleteShellText text of shell
+	 * 
+	 * @param deleteShellText
+	 *            text of shell
 	 */
 	public static void handleDeletion(org.jboss.reddeer.swt.api.Shell deleteShell) {
 		handleDeletion(deleteShell, TimePeriod.VERY_LONG);
 	}
-	
+
 	/**
 	 * Handles specific cases during project deletion
-	 * @param deleteShellText text of shell
-	 * @param timeout timeout
+	 * 
+	 * @param deleteShellText
+	 *            text of shell
+	 * @param timeout
+	 *            timeout
 	 */
-	public static void handleDeletion(org.jboss.reddeer.swt.api.Shell deleteShell,TimePeriod timeout) {
+	public static void handleDeletion(org.jboss.reddeer.swt.api.Shell deleteShell, TimePeriod timeout) {
+		new WaitUntil(new ShellHasChildrenOrIsNotAvailable(deleteShell), timeout);
+		int childShells = 0;
 		try{
-			deleteShell.setFocus();
+			childShells = ShellHandler.getInstance().getShells(deleteShell.getSWTWidget()).length;
 		} catch (SWTLayerException ex){
-			//delete shell is disposed
-		} finally{
-			new WaitWhile(new DeleteShellIsActive(deleteShell), timeout);
-			try{
-				new WaitUntil(new DeleteShellIsNotVisible(deleteShell),TimePeriod.NORMAL);
-			} catch (WaitTimeoutExpiredException ex){
+			log.debug("Delete shell is disposed.");
+		} finally {
+			if (childShells == 1) {
 				new ShellWithButton("Delete Resources", "Continue");
 				new PushButton("Continue").click();
-				new WaitWhile(new ShellWithTextIsAvailable("Delete Resources"),timeout);
+				new WaitWhile(new ShellWithTextIsAvailable("Delete Resources"), timeout);
 			}
 			new WaitWhile(new JobIsRunning(), timeout);
 		}
 	}
-	
+
 	/**
 	 * Deletes project via Eclipse API in case deleting via UI calls failed
-	 * @param deleteShellText text of shell
+	 * 
+	 * @param deleteShellText
+	 *            text of shell
 	 */
-	public static void forceProjectDeletion(Project project , boolean deleteFromFileSystem) {
+	public static void forceProjectDeletion(Project project, boolean deleteFromFileSystem) {
 		try {
 			project.delete(deleteFromFileSystem);
 		} catch (EclipseLayerException ele) {
 			log.debug("Delete project '" + project.getName() + "' via Eclipse API ");
-			org.jboss.reddeer.direct.project.Project.delete(project.getName(),
-					deleteFromFileSystem, true);
+			org.jboss.reddeer.direct.project.Project.delete(project.getName(), deleteFromFileSystem, true);
 		}
-		
+
 	}
-	
+
 	private static class ShellWithButton extends AbstractShell {
 
 		public ShellWithButton(String title, String buttonLabel) {
 			super(lookForShellWithButton(title, buttonLabel));
 			setFocus();
-			log.debug("Shell with title '" + title + "' and button '"
-					+ buttonLabel + "' found");
-		}
-
-	}
-	
-	private static class DeleteShellIsNotVisible implements WaitCondition{
-		
-		private org.jboss.reddeer.swt.api.Shell shell;
-		
-		public DeleteShellIsNotVisible(org.jboss.reddeer.swt.api.Shell shell) {
-			this.shell = shell;
-		}
-
-		@Override
-		public boolean test() {
-			try{
-				return ! shell.isVisible();
-			} catch (SWTLayerException ex){
-				return true;
-			}
-		}
-
-		@Override
-		public String description() {
-			return "Delete Shell is visible.";
-		}
-		
-	}
-	
-	private static class DeleteShellIsActive implements WaitCondition{
-		
-		private org.jboss.reddeer.swt.api.Shell shell;
-		
-		public DeleteShellIsActive(org.jboss.reddeer.swt.api.Shell shell) {
-			this.shell = shell;
-		}
-
-		@Override
-		public boolean test() {
-			try{
-				return shell.isFocused();
-			} catch (SWTLayerException ex){
-				return false;
-			}
-		}
-
-		@Override
-		public String description() {
-			return "Delete shell is active.";  
+			log.debug("Shell with title '" + title + "' and button '" + buttonLabel + "' found");
 		}
 
 	}
 
-	private static Shell lookForShellWithButton(final String title,
-			final String buttonLabel) {
+	private static Shell lookForShellWithButton(final String title, final String buttonLabel) {
 		Matcher<String> titleMatcher = new WithTextMatcher(title);
 		Matcher<String> buttonMatcher = new BaseMatcher<String>() {
 			@Override
@@ -156,13 +112,12 @@ public class DeleteUtils {
 
 			@Override
 			public void describeTo(Description description) {
-				description.appendText("containing button '" + buttonLabel
-						+ "'");
+				description.appendText("containing button '" + buttonLabel + "'");
 			}
 		};
 		@SuppressWarnings("unchecked")
 		Matcher<String> matcher = new AndMatcher(titleMatcher, buttonMatcher);
 		return ShellLookup.getInstance().getShell(matcher);
 	}
-	
+
 }
---
+++ plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/condition/ShellHasChildrenOrIsNotAvailable.java
@@ -0,0 +1,48 @@
+package org.jboss.reddeer.swt.condition;
+
+import org.jboss.reddeer.common.logging.Logger;
+import org.jboss.reddeer.swt.api.Shell;
+import org.jboss.reddeer.swt.exception.SWTLayerException;
+import org.jboss.reddeer.swt.handler.ShellHandler;
+
+/**
+ * Condition is met when shell has children or is not available anymore
+ * @author rawagner
+ *
+ */
+public class ShellHasChildrenOrIsNotAvailable implements WaitCondition{
+	
+	private Shell shell;
+	private static final Logger log = Logger.getLogger(ShellHasChildrenOrIsNotAvailable.class);
+
+	/**
+	 * Default constructor.
+	 * @param deleteShell instance of shell to test
+	 */
+	public ShellHasChildrenOrIsNotAvailable(Shell deleteShell) {
+		this.shell = deleteShell;
+	}
+
+	@Override
+	public boolean test() {
+		int childShells = 0;
+		org.eclipse.swt.widgets.Shell swtShell = shell.getSWTWidget();
+		ShellHandler handler = ShellHandler.getInstance();
+		try {
+			childShells = handler.getShells(swtShell).length;
+		} catch (SWTLayerException e) {
+			if(swtShell != null){
+				return handler.isDisposed(swtShell);
+			}
+			return true;
+		}
+		log.debug("number of child shells: " + childShells);
+		return childShells > 0;
+	}
+
+	@Override
+	public String description() {
+		return "Shell has children or is not available.";
+	}
+
+}
