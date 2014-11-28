c689f8e9f3c96ad47f776afe7b269c980a9c99c2
rawagner <rawagner@redhat.com>
2014-11-28 09:36:38 +0100

    Add logging to content assist #890

--- plugins/org.jboss.reddeer.jface/src/org/jboss/reddeer/jface/text/contentassist/ContentAssistant.java
+++ plugins/org.jboss.reddeer.jface/src/org/jboss/reddeer/jface/text/contentassist/ContentAssistant.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
+import org.jboss.reddeer.common.logging.Logger;
 import org.jboss.reddeer.swt.api.Table;
 import org.jboss.reddeer.swt.api.TableItem;
 import org.jboss.reddeer.swt.handler.ShellHandler;
@@ -17,6 +18,8 @@ import org.jboss.reddeer.swt.lookup.ShellLookup;
  * @since 0.6
  */
 public class ContentAssistant {
+	
+	protected static final Logger log = Logger.getLogger(ContentAssistant.class);
 	
 	private Table contentAssistTable;
 	private Shell contentAssistShell;
@@ -35,9 +38,13 @@ public class ContentAssistant {
 	 * @return list of proposals
 	 */
 	public List<String> getProposals(){
+		log.debug("Getting content assist proposals");
 		List<String> proposals = new ArrayList<String>();
+		log.debug("Content assist proposals:");
 		for(TableItem i: contentAssistTable.getItems()){
-			proposals.add(i.getText());
+			String text = i.getText();
+			log.debug("  "+text);
+			proposals.add(text);
 		}
 		return proposals;
 	}
@@ -47,6 +54,7 @@ public class ContentAssistant {
 	 * @param proposal to select
 	 */
 	public void chooseProposal(String proposal){
+		log.debug("Choose content assist proposal - "+proposal);
 		contentAssistTable.getItem(proposal).select();
 		KeyboardFactory.getKeyboard().invokeKeyCombination(SWT.CR);
 	}
@@ -55,6 +63,7 @@ public class ContentAssistant {
 	 * Closes content assist shell
 	 */
 	public void close(){
+		log.debug("Closing content assist");
 		if(contentAssistShell != null){
 			ShellHandler.getInstance().closeShell(contentAssistShell);
 		}
