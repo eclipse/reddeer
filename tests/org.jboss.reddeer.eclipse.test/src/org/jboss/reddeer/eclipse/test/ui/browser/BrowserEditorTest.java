5bbfef3cf98d6bf1b398b4d27d10ca87b36922a9
vpakan <vpakan@redhat.com>
2015-01-16 12:28:37 +0100

    Fixes BrowserEditorTest. Handles exceptions properly. Fixes #917

--- tests/org.jboss.reddeer.eclipse.test/src/org/jboss/reddeer/eclipse/test/ui/browser/BrowserEditorTest.java
+++ tests/org.jboss.reddeer.eclipse.test/src/org/jboss/reddeer/eclipse/test/ui/browser/BrowserEditorTest.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+import org.jboss.reddeer.eclipse.exception.EclipseLayerException;
 import org.jboss.reddeer.eclipse.ui.browser.BrowserEditor;
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.reddeer.swt.util.Display;
@@ -18,30 +19,36 @@ import org.junit.runner.RunWith;
 
 @RunWith(RedDeerSuite.class)
 public class BrowserEditorTest {
-	
+
 	@Before
-	public void openBrowser(){
+	public void openBrowser() {
 		Display.syncExec(new Runnable() {
 
 			@Override
 			public void run() {
 				try {
-					PlatformUI.getWorkbench().getBrowserSupport().createBrowser(IWorkbenchBrowserSupport.AS_EDITOR  & IWorkbenchBrowserSupport.NAVIGATION_BAR & IWorkbenchBrowserSupport.LOCATION_BAR,null,"a","b").openURL(new URL("http://www.redhat.com"));
-				} catch (PartInitException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					PlatformUI
+						.getWorkbench()
+						.getBrowserSupport()
+						.createBrowser(
+								IWorkbenchBrowserSupport.AS_EDITOR
+								| IWorkbenchBrowserSupport.NAVIGATION_BAR
+								| IWorkbenchBrowserSupport.LOCATION_BAR,
+								null, "a", "b")
+						.openURL(new URL("http://www.redhat.com"));
+				} catch (PartInitException pie) {
+					throw new EclipseLayerException("Unable to create browser editor", pie);
+				} catch (MalformedURLException mue) {
+					throw new EclipseLayerException("URL opening in browser is malformed", mue);
 				}
 			}
 		});
 	}
-	
+
 	@Test
 	public void testEditor() {
 		new DefaultEditor("a");
-		assertEquals("b",new BrowserEditor("a").getTitleToolTip());
+		assertEquals("b", new BrowserEditor("a").getTitleToolTip());
 	}
 
 }
