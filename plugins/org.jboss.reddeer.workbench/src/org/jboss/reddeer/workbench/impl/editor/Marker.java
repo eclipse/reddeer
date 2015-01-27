45a669f3612dc5a2517d892b6c798922e633ebfc
vpakan <vpakan@redhat.com>
2015-01-27 13:53:50 +0100

    Fixes EditorHandler.getMarkers() ot handle as you type markers properly. Fixes #929

--- plugins/org.jboss.reddeer.workbench/src/org/jboss/reddeer/workbench/impl/editor/Marker.java
+++ plugins/org.jboss.reddeer.workbench/src/org/jboss/reddeer/workbench/impl/editor/Marker.java
@@ -63,4 +63,45 @@ public class Marker {
 	public int getLineNumber() {
 		return lineNumber;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + lineNumber;
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Marker other = (Marker) obj;
+		if (lineNumber != other.lineNumber)
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Marker [text=" + text + ", type=" + type + ", lineNumber="
+				+ lineNumber + "]";
+	}	
+	
 }
