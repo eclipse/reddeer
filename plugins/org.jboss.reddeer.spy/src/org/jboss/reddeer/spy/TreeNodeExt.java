d45881722006a5ff00c30fc2c4a511c52b8f8ad8
Andrej Podhradsky <apodhrad@redhat.com>
2015-02-09 14:57:51 +0100

    Create GEF viewer #907

---
+++ plugins/org.jboss.reddeer.spy/src/org/jboss/reddeer/spy/TreeNodeExt.java
@@ -0,0 +1,49 @@
+package org.jboss.reddeer.spy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.viewers.TreeNode;
+
+/**
+ * Extended TreeNode by implementing children as collection instead of array.
+ * 
+ * @author apodhrad
+ *
+ */
+public class TreeNodeExt extends TreeNode {
+
+	private List<TreeNode> children;
+
+	/**
+	 * Constructs a new instance of <code>TreeNodeExt</code>.
+	 * 
+	 * @param value
+	 *            The value held by this node; may be anything.
+	 */
+	public TreeNodeExt(Object value) {
+		super(value);
+		children = new ArrayList<TreeNode>();
+	}
+
+	/**
+	 * Adds a child node.
+	 * 
+	 * @param node
+	 *            Child node
+	 */
+	public void addChild(TreeNode node) {
+		children.add(node);
+	}
+
+	@Override
+	public TreeNode[] getChildren() {
+		return (TreeNode[]) children.toArray(new TreeNode[children.size()]);
+	}
+
+	@Override
+	public boolean hasChildren() {
+		return !children.isEmpty();
+	}
+
+}
