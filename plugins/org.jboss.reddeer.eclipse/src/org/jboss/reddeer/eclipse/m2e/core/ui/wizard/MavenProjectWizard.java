c2554eb601943dfa9ca88154da4c6eb65a6e3b26
rawagner <rawagner@redhat.com>
2015-03-09 16:37:19 +0100

    Added New Maven Projec Wizard. #963

---
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/m2e/core/ui/wizard/MavenProjectWizard.java
@@ -0,0 +1,22 @@
+package org.jboss.reddeer.eclipse.m2e.core.ui.wizard;
+
+import org.jboss.reddeer.jface.wizard.NewWizardDialog;
+
+/**
+ * New Maven Project Wizard Dialog
+ * @author rawagner
+ *
+ */
+public class MavenProjectWizard extends NewWizardDialog{
+	
+	public static final String CATEGORY="Maven";
+	public static final String NAME="Maven Project";
+	
+	/**
+	 * Default constructor
+	 */
+	public MavenProjectWizard(){
+		super(CATEGORY,NAME);
+	}
+
+}
