0ae90234bc9c9600ba5dbee99098b6b9666f9e75
Jiri Peterka <peterka.jiri@gmail.com>
2015-03-13 09:29:16 +0100

    Step log type support added into RedDeer logger

--- plugins/org.jboss.reddeer.common/src/org/jboss/reddeer/common/logging/MessageType.java
+++ plugins/org.jboss.reddeer.common/src/org/jboss/reddeer/common/logging/MessageType.java
@@ -15,5 +15,6 @@ public class MessageType {
 	public static final int WARN = 16;
 	public static final int ERROR = 32;
 	public static final int FATAL = 64;
-	public static final int ALL = DUMP | TRACE | DEBUG | INFO | WARN | ERROR | FATAL;
+	public static final int STEP = 128;
+	public static final int ALL = DUMP | TRACE | DEBUG | INFO | WARN | ERROR | FATAL | STEP;
 }
