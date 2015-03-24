d520ba71aa11a8c6f66ef3f74000ac912f03f145
rhopp <rhopp@redhat.com>
2015-03-24 12:51:37 +0100

    Condition for waiting for particular shell added.
    Fixes #993
    
    Signed-off-by: rhopp <rhopp@redhat.com>

--- plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/condition/ShellIsActive.java
+++ plugins/org.jboss.reddeer.swt/src/org/jboss/reddeer/swt/condition/ShellIsActive.java
@@ -1,18 +1,55 @@
 package org.jboss.reddeer.swt.condition;
 
+import org.jboss.reddeer.common.logging.Logger;
+import org.jboss.reddeer.swt.api.Shell;
 import org.jboss.reddeer.swt.lookup.ShellLookup;
+import org.jboss.reddeer.swt.util.internal.InstanceValidator;
 
 /**
- * Condition is met when shell is active.
+ * Wait condition for shells checking whether some shell is active (empty
+ * constructor) or using Shell.equals (parameterized constructor).
  * 
  * @author mlabuda
  *
  */
 public class ShellIsActive implements WaitCondition {
 	
+	private Shell shell;
+	private static final Logger log = Logger.getLogger(ShellIsActive.class);
+	
+	
+	/**
+	 * Fulfilled, when there is some shell active.
+	 * 
+	 */
+	public ShellIsActive() {
+		this.shell=null;
+	}
+	
+	
+	/**
+	 * Fulfilled, when active shell is equal to given shell.
+	 * 
+	 * @param shell Shell to compare to.
+	 */
+	public ShellIsActive(Shell shell){
+		InstanceValidator.checkNotNull(shell, "shell");
+		this.shell = shell;
+	}
+	
 	@Override
 	public boolean test() {
-		return ShellLookup.getInstance().getCurrentActiveShell() != null;
+		if (shell == null){
+			return ShellLookup.getInstance().getCurrentActiveShell() != null;
+		}else{
+			org.eclipse.swt.widgets.Shell currentActiveShell = ShellLookup.getInstance()
+					.getCurrentActiveShell();
+			if (currentActiveShell == null) {
+				log.debug("Current active shell is null");
+				return false;
+			}
+			return currentActiveShell.equals(shell.getSWTWidget());
+		}
 	}
 
 	@Override
