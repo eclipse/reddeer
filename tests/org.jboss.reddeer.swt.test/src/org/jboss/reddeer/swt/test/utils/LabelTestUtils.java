52adfda8bd273661488d33e3f305d21b16d593cf
Andrej Podhradsky <apodhrad@redhat.com>
2015-07-03 17:09:34 +0200

    Improve algorithm for find a labeled widget #1126

--- tests/org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/utils/LabelTestUtils.java
+++ tests/org.jboss.reddeer.swt.test/src/org/jboss/reddeer/swt/test/utils/LabelTestUtils.java
@@ -3,25 +3,25 @@ package org.jboss.reddeer.swt.test.utils;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Shell;
 
 public class LabelTestUtils {
 
-	public static Label createLabel(Shell parent, String text){
+	public static Label createLabel(Composite parent, String text){
 		Label swtLabel= new Label(parent, SWT.LEFT);
 		swtLabel.setText(text);
 		swtLabel.setSize(100,30);
 		return swtLabel;
 	}
 
-	public static Label createLabel(Shell parent, String text, Image image){
+	public static Label createLabel(Composite parent, String text, Image image){
 		Label swtLabel= createLabel(parent, text);
 		swtLabel.setImage(image);
 		return swtLabel;
 	}
 	
-	public static CLabel createCLabel(Shell parent, String text){
+	public static CLabel createCLabel(Composite parent, String text){
 		CLabel swtLabel= new CLabel(parent, SWT.LEFT);
 		swtLabel.setText(text);
 		swtLabel.setSize(100,30);
