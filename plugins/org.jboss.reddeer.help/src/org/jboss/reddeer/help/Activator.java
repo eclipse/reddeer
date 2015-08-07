1a935014eb6ae240dacee7855ed9829ca345330f
mlabuda <mlabuda@redhat.com>
2015-08-07 17:11:23 +0200

    Create RedDeer help plugin (fixes #1196)

---
+++ plugins/org.jboss.reddeer.help/src/org/jboss/reddeer/help/Activator.java
@@ -0,0 +1,52 @@
+package org.jboss.reddeer.help;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+        // The plug-in ID
+        public static final String PLUGIN_ID = "org.jboss.reddeer.help";
+
+        // The shared instance
+        private static Activator plugin;
+        
+        boolean started;
+        /**
+         * The constructor
+         */
+        public Activator() {
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+         */
+        public void start(BundleContext context) throws Exception {
+                super.start(context);
+                plugin  = this;
+                started = true;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+         */
+        public void stop(BundleContext context) throws Exception {
+                plugin = null;
+                started = false;
+                super.stop(context);
+        }
+
+        /**
+         * Returns the shared instance
+         *
+         * @return the shared instance
+         */
+        public static Activator getDefault() {
+                return plugin;
+        }
+}
