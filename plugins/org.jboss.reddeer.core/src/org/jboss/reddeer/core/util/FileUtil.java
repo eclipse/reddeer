5544ca3ad1625f494db131ddc19f1718d842dee8
psrna <psrna@redhat.com>
2015-08-12 08:01:29 +0200

    FileUtil for reading a File into a String (fixes #1197).

---
+++ plugins/org.jboss.reddeer.core/src/org/jboss/reddeer/core/util/FileUtil.java
@@ -0,0 +1,35 @@
+package org.jboss.reddeer.core.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+
+/**
+ * Provides methods for File manipulation.
+ * @author psrna
+ *
+ */
+public class FileUtil {
+
+	/**
+	 * Read File into a String.
+	 * @param path
+	 * @return
+	 * @throws IOException
+	 */
+	public static String readFile(String path) throws IOException {
+		FileInputStream stream = new FileInputStream(new File(path));
+		try {
+			FileChannel fc = stream.getChannel();
+			MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+			/* Instead of using default, pass in a decoder. */
+			return Charset.defaultCharset().decode(bb).toString();
+		} finally {
+			stream.close();
+		}
+	}
+
+}
