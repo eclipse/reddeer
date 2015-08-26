c460725effb4cc6da11e9ebbc964caa3e4f529d5
vpakan <vpakan@redhat.com>
2015-08-26 13:46:28 +0200

    ProjectExists condition supports AbstractExplorer as input parameter. Fixes #1213

--- plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/condition/ProjectExists.java
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/condition/ProjectExists.java
@@ -1,5 +1,6 @@
 package org.jboss.reddeer.eclipse.condition;
 
+import org.jboss.reddeer.eclipse.jdt.ui.AbstractExplorer;
 import org.jboss.reddeer.eclipse.jdt.ui.packageexplorer.PackageExplorer;
 import org.jboss.reddeer.common.condition.WaitCondition;
 
@@ -11,27 +12,33 @@ import org.jboss.reddeer.common.condition.WaitCondition;
 public class ProjectExists implements WaitCondition{
 	
 	private String projectName;
+	private AbstractExplorer explorer;
 	
 	/**
 	 * Default constructor
 	 * @param projectName name of the project to find
 	 */
 	public ProjectExists(String projectName) {
+		this(projectName , new PackageExplorer());
+	}
+	
+	/**
+	 * Constructor with specified view
+	 * @param projectName name of the project to find
+	 */
+	public ProjectExists(String projectName , AbstractExplorer explorer) {
 		this.projectName = projectName;
+		this.explorer = explorer;
 	}
 
 	@Override
 	public boolean test() {
-		PackageExplorer pe = new PackageExplorer();
-		pe.open();
-		return pe.containsProject(projectName);
+		explorer.open();
+		return explorer.containsProject(projectName);
 	}
 
 	@Override
 	public String description() {
 		return "Project "+projectName+" exists.";
 	}
-	
-	
-
 }
