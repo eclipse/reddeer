19f70e1b2a9e1a15856cedd66191c8ff26d20e13
ljelinkova <ljelinko@redhat.com>
2015-10-27 14:45:45 +0100

    Refactored ParametrizedRunner #1280

--- plugins/org.jboss.reddeer.junit/src/org/jboss/reddeer/junit/internal/runner/ParameterizedRunner.java
+++ plugins/org.jboss.reddeer.junit/src/org/jboss/reddeer/junit/internal/runner/ParameterizedRunner.java
@@ -2,21 +2,12 @@ package org.jboss.reddeer.junit.internal.runner;
 
 import java.util.List;
 
-import org.jboss.reddeer.common.logging.Logger;
 import org.jboss.reddeer.junit.extensionpoint.IAfterTest;
 import org.jboss.reddeer.junit.extensionpoint.IBeforeTest;
 import org.jboss.reddeer.junit.internal.requirement.Requirements;
-import org.jboss.reddeer.junit.internal.requirement.inject.RequirementsInjector;
-import org.jboss.reddeer.junit.internal.runner.statement.CleanUpRequirementStatement;
-import org.jboss.reddeer.junit.internal.runner.statement.FulfillRequirementsStatement;
-import org.jboss.reddeer.junit.internal.runner.statement.RunAfters;
-import org.jboss.reddeer.junit.internal.runner.statement.RunBefores;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunListener;
 import org.junit.runners.Parameterized;
-import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 
 /**
@@ -30,18 +21,19 @@ import org.junit.runners.model.Statement;
 
 public class ParameterizedRunner extends Parameterized {
 
-	private RequirementsInjector requirementsInjector = new RequirementsInjector();
-	private static final Logger log = Logger.getLogger(ParameterizedRunner.class);
-
 	private String configId;
 	private Requirements requirements;
 	private RunListener[] runListeners;
 	private List<IBeforeTest> beforeTestExtensions;
 	private List<IAfterTest> afterTestExtensions;
 
+	private RequirementsRunner requirementsRunner;
+	
 	public ParameterizedRunner(Class<?> clazz, Requirements requirements, String configId, RunListener[] runListeners,
 			List<IBeforeTest> beforeTestExtensions, List<IAfterTest> afterTestExtensions) throws Throwable {
 		super(clazz);
+		this.requirementsRunner = new RequirementsRunner(clazz, requirements, configId, runListeners, beforeTestExtensions, afterTestExtensions);
+		
 		this.requirements = requirements;
 		this.configId = configId;
 		this.runListeners = runListeners;
@@ -74,48 +66,11 @@ public class ParameterizedRunner extends Parameterized {
 
 	@Override
 	protected Statement withBeforeClasses(Statement statement) {
-		log.debug("Injecting fulfilled requirements into static fields of test class");
-		requirementsInjector.inject(getTestClass().getJavaClass(), requirements);
-
-		List<FrameworkMethod> befores = getTestClass().getAnnotatedMethods(BeforeClass.class);
-		Statement s = befores.isEmpty() ? statement
-				: new RunBefores(configId, statement, getTestClass(), befores);
-		runBeforeTestClass();
-		return new FulfillRequirementsStatement(requirements, s);
+		return requirementsRunner.withBeforeClasses(statement);
 	}
 
 	@Override
-	protected Statement withAfterClasses(Statement statement) {
-		List<FrameworkMethod> afters = getTestClass().getAnnotatedMethods(AfterClass.class);
-		Statement s = afters.isEmpty() ? statement
-				: new RunAfters(configId, statement, getTestClass(), afters);
-		runAfterTest();
-		return new CleanUpRequirementStatement(requirements, s);
-	}
-
-	/**
-	 * Method is called before test is run. Manages
-	 * org.jboss.reddeer.junit.extensionpoint.IBeforeTest extensions
-	 */
-	private void runBeforeTestClass() {
-		for (IBeforeTest beforeTestExtension : beforeTestExtensions) {
-			if (beforeTestExtension.hasToRun()) {
-				log.debug("Run method runBeforeTest() of class " + beforeTestExtension.getClass().getCanonicalName());
-				beforeTestExtension.runBeforeTestClass(configId, getTestClass());
-			}
-		}
-	}
-
-	/**
-	 * Method is called after test is run. Manages
-	 * org.jboss.reddeer.junit.extensionpoint.IAfterTest extensions
-	 */
-	private void runAfterTest() {
-		for (IAfterTest afterTestExtension : afterTestExtensions) {
-			if (afterTestExtension.hasToRun()) {
-				log.debug("Run method runAfterTest() of class " + afterTestExtension.getClass().getCanonicalName());
-				afterTestExtension.runAfterTestClass(configId, getTestClass());
-			}
-		}
-	}
+	 protected Statement withAfterClasses(Statement statement) {
+      return requirementsRunner.withAfterClasses(statement);
+   }
 }
