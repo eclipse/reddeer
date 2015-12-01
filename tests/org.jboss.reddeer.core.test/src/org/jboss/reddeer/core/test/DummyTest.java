c9960d48f59f27e2afa85c41d83cfdc5e5a3b89d
mlabuda <mlabuda@redhat.com>
2015-12-01 16:37:41 +0100

    Remove ViewWithTitleIsActive wait condition (fixes #1319)

---
+++ tests/org.jboss.reddeer.core.test/src/org/jboss/reddeer/core/test/DummyTest.java
@@ -0,0 +1,18 @@
+package org.jboss.reddeer.core.test;
+
+import org.junit.Test;
+
+/**
+ * Dummy test to have at least one test in core test plugin. Once there will be any test,
+ * this one can be removed.
+ * 
+ * @author mlabuda@redhat.com
+ *
+ */
+public class DummyTest {
+
+	@Test
+	public void dummyTest() {
+		
+	}
+}
