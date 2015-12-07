6ca471accc31465e5668f42817d0a1cd0c11d6a1
Jan Richter <jrichter@redhat.com>
2015-12-07 15:09:35 +0100

    Make NewServerWizardPage wait until server tree updates #1322

--- plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/wst/server/ui/wizard/NewServerWizardPage.java
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/wst/server/ui/wizard/NewServerWizardPage.java
@@ -1,7 +1,11 @@
 package org.jboss.reddeer.eclipse.wst.server.ui.wizard;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.core.StringContains;
 import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitUntil;
+import org.jboss.reddeer.common.wait.WaitWhile;
+import org.jboss.reddeer.core.condition.JobIsRunning;
 import org.jboss.reddeer.jface.wizard.WizardPage;
 import org.jboss.reddeer.swt.condition.TreeHasSelectedItems;
 import org.jboss.reddeer.swt.impl.text.LabeledText;
@@ -9,7 +13,8 @@ import org.jboss.reddeer.swt.impl.tree.DefaultTree;
 import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
 
 /**
- * Represents the first page displayed when invoked {@link NewServerWizardDialog}
+ * Represents the first page displayed when invoked
+ * {@link NewServerWizardDialog}
  * 
  * @author Lucia Jelinkova
  *
@@ -19,28 +24,33 @@ public class NewServerWizardPage extends WizardPage {
 	/**
 	 * Select type.
 	 *
-	 * @param type the type
+	 * @param type
+	 *            the type
 	 */
-	public void selectType(String... type){
-		new WaitUntil(new TreeHasSelectedItems(new DefaultTree()),TimePeriod.NORMAL,false);
+	public void selectType(String... type) {
+		new WaitUntil(new TreeHasSelectedItems(new DefaultTree()), TimePeriod.NORMAL, false);
+		new WaitWhile(new JobIsRunning(new Matcher[] { new StringContains("Decoration") }, null, false),
+				TimePeriod.NORMAL, false);
 		new DefaultTreeItem(type).select();
 	}
 
 	/**
 	 * Sets the name.
 	 *
-	 * @param name the new name
+	 * @param name
+	 *            the new name
 	 */
-	public void setName(String name){
+	public void setName(String name) {
 		new LabeledText("Server name:").setText(name);
 	}
 
 	/**
 	 * Sets the host name.
 	 *
-	 * @param name the new host name
+	 * @param name
+	 *            the new host name
 	 */
-	public void setHostName(String name){
+	public void setHostName(String name) {
 		new LabeledText("Server's host name:").setText(name);
 	}
 }
