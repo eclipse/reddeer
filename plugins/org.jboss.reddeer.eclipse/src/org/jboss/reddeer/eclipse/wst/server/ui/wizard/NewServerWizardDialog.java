49885633639674239272fcf15322db14bcdefb8d
Rastislav Wagner <rawagner@redhat.com>
2016-07-13 09:03:33 +0200

    Wait for Refreshing job to start. Fixes #1409

--- plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/wst/server/ui/wizard/NewServerWizardDialog.java
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/wst/server/ui/wizard/NewServerWizardDialog.java
@@ -10,9 +10,12 @@
  ******************************************************************************/ 
 package org.jboss.reddeer.eclipse.wst.server.ui.wizard;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.core.IsEqual;
 import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitUntil;
 import org.jboss.reddeer.core.condition.JobIsKilled;
+import org.jboss.reddeer.core.condition.JobIsRunning;
 import org.jboss.reddeer.jface.wizard.NewWizardDialog;
 
 /**
@@ -26,6 +29,8 @@ public class NewServerWizardDialog extends NewWizardDialog {
 
 	public static final String TITLE = "New Server";
 	
+	private static final String REFRESHING_JOB_NAME = "Refreshing server adapter list";
+	
 	/**
 	 * Instantiates a new new server wizard dialog.
 	 */
@@ -36,14 +41,20 @@ public class NewServerWizardDialog extends NewWizardDialog {
 	@Override
 	public void finish(TimePeriod timeout) {
 		// workaround due to JBDS-3596
-		new WaitUntil(new JobIsKilled("Refreshing server adapter list"), TimePeriod.LONG, false);
+		Matcher[] jobs = new Matcher[1];
+		jobs[0] = new IsEqual<String>(REFRESHING_JOB_NAME);
+		new WaitUntil(new JobIsRunning(jobs),TimePeriod.NORMAL,false);
+		new WaitUntil(new JobIsKilled(REFRESHING_JOB_NAME), TimePeriod.LONG, false);
 		super.finish(timeout);
 	}
 
 	@Override
 	public void cancel() {
 		// workaround due to JBDS-3596
-		new WaitUntil(new JobIsKilled("Refreshing server adapter list"), TimePeriod.LONG, false);
+		Matcher[] jobs = new Matcher[1];
+		jobs[0] = new IsEqual<String>(REFRESHING_JOB_NAME);
+		new WaitUntil(new JobIsRunning(jobs),TimePeriod.NORMAL,false);
+		new WaitUntil(new JobIsKilled(REFRESHING_JOB_NAME), TimePeriod.LONG, false);
 		super.cancel();
 	}
 	
