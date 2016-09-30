f4abaabdb410043f2170e2d26a1ffd896066de70
Rastislav Wagner <rawagner@redhat.com>
2016-09-30 11:59:36 +0200

    Modify Modules dialog may ask for confirmation. Fixes #1488

--- plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/wst/server/ui/wizard/ModifyModulesDialog.java
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/wst/server/ui/wizard/ModifyModulesDialog.java
@@ -10,7 +10,17 @@
  ******************************************************************************/ 
 package org.jboss.reddeer.eclipse.wst.server.ui.wizard;
 
+import org.jboss.reddeer.common.exception.RedDeerException;
+import org.jboss.reddeer.common.logging.Logger;
+import org.jboss.reddeer.common.wait.TimePeriod;
+import org.jboss.reddeer.common.wait.WaitWhile;
+import org.jboss.reddeer.core.condition.JobIsRunning;
 import org.jboss.reddeer.jface.wizard.WizardDialog;
+import org.jboss.reddeer.swt.api.Shell;
+import org.jboss.reddeer.swt.condition.ShellIsAvailable;
+import org.jboss.reddeer.swt.impl.button.FinishButton;
+import org.jboss.reddeer.swt.impl.button.OkButton;
+import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 
 /**
  * Wizard for adding and removing modules on the server. 
@@ -20,5 +30,28 @@ import org.jboss.reddeer.jface.wizard.WizardDialog;
  */
 public class ModifyModulesDialog extends WizardDialog {
 
+	protected final Logger log = Logger.getLogger(this.getClass());
 	public static final String DIALOG_TITLE = "Add and Remove...";
+	
+	/**
+	 * Click the finish button in wizard dialog.
+	 * @param timeout to wait for wizard shell to close.
+	 */
+	public void finish(TimePeriod timeout) {
+		log.info("Finish wizard");
+		
+		Shell s = new DefaultShell(DIALOG_TITLE);
+		new FinishButton().click();
+		
+		try{
+			Shell serverShell = new DefaultShell("Server");
+			new OkButton().click();
+			new WaitWhile(new ShellIsAvailable(serverShell));
+		} catch (RedDeerException e) {
+			log.debug("Dialog '"+DIALOG_TITLE+"' did not ask for confirmation");
+		}
+	
+		new WaitWhile(new ShellIsAvailable(s), timeout);
+		new WaitWhile(new JobIsRunning(), timeout);
+	}
 }
