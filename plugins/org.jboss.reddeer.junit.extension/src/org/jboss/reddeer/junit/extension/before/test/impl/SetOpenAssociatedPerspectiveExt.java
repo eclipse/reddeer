cab930f12a52d65e8b8fe488312faf86492be870
Andrej Podhradsky <apodhrad@redhat.com>
2016-10-12 16:33:11 +0200

    Use direct plugin for setting preferences as much as possible #1364

--- plugins/org.jboss.reddeer.junit.extension/src/org/jboss/reddeer/junit/extension/before/test/impl/SetOpenAssociatedPerspectiveExt.java
+++ plugins/org.jboss.reddeer.junit.extension/src/org/jboss/reddeer/junit/extension/before/test/impl/SetOpenAssociatedPerspectiveExt.java
@@ -10,14 +10,11 @@
  ******************************************************************************/ 
 package org.jboss.reddeer.junit.extension.before.test.impl;
 
-import org.eclipse.core.runtime.Platform;
 import org.jboss.reddeer.common.logging.Logger;
 import org.jboss.reddeer.common.properties.RedDeerProperties;
-import org.jboss.reddeer.eclipse.ui.dialogs.PerspectivesPreferencePage;
+import org.jboss.reddeer.direct.preferences.PreferencesUtil;
 import org.jboss.reddeer.junit.extension.ExtensionPriority;
 import org.jboss.reddeer.junit.extensionpoint.IBeforeTest;
-import org.jboss.reddeer.swt.exception.SWTLayerException;
-import org.jboss.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.TestClass;
 
@@ -31,12 +28,12 @@ import org.junit.runners.model.TestClass;
  * (default=never)
  * 
  * @author vlado pakan
+ * @author Andrej Podhradsky (apodhrad@redhat.com)
  *
  */
 public class SetOpenAssociatedPerspectiveExt implements IBeforeTest {
 
-	private static final Logger log = Logger
-			.getLogger(SetOpenAssociatedPerspectiveExt.class);
+	private static final Logger log = Logger.getLogger(SetOpenAssociatedPerspectiveExt.class);
 
 	
 	@Override
@@ -56,41 +53,9 @@ public class SetOpenAssociatedPerspectiveExt implements IBeforeTest {
 	 * Sets Open Associated Perspective when project is created.
 	 */
 	private void setOpenAssociatedPerspective() {
-		String openAssociatedPerspective = Platform.getPreferencesService()
-				.getString("org.eclipse.ui.ide",
-						"SWITCH_PERSPECTIVE_ON_PROJECT_CREATION", "prompt",
-						null);
-		if (!openAssociatedPerspective
-				.equalsIgnoreCase(RedDeerProperties.OPEN_ASSOCIATED_PERSPECTIVE.getValue())) {
-			log.debug("Setting open associated perspective to: "
-					+ RedDeerProperties.OPEN_ASSOCIATED_PERSPECTIVE.getValue()
-					+ " via Windows > Preferences > General > Perspectives");
-
-			WorkbenchPreferenceDialog preferencesDialog = new WorkbenchPreferenceDialog();
-			PerspectivesPreferencePage perspectivesPreferencePage = new PerspectivesPreferencePage();
-			preferencesDialog.open();
-			preferencesDialog.select(perspectivesPreferencePage);
-			if (RedDeerProperties.OPEN_ASSOCIATED_PERSPECTIVE
-					.getValue().equalsIgnoreCase("never")) {
-				perspectivesPreferencePage
-						.checkNeverOpenAssociatedPerspective();
-			} else if (RedDeerProperties.OPEN_ASSOCIATED_PERSPECTIVE
-					.getValue().equalsIgnoreCase("always")) {
-				perspectivesPreferencePage
-						.checkAlwaysOpenAssociatedPerspective();
-			} else if (RedDeerProperties.OPEN_ASSOCIATED_PERSPECTIVE
-					.getValue().equalsIgnoreCase("prompt")) {
-				perspectivesPreferencePage
-						.checkPromptOpenAssociatedPerspective();
-			} else {
-				throw new SWTLayerException(
-						"Invalid paramter value used: "
-								+ "rd.openAssociatedPerspective = "
-								+ RedDeerProperties.OPEN_ASSOCIATED_PERSPECTIVE.getValue());
-			}
-			preferencesDialog.ok();
-
-		}
+		String openAssociatedPerspective = RedDeerProperties.OPEN_ASSOCIATED_PERSPECTIVE.getValue();
+		log.debug("Setting open associated perspective to: '" + openAssociatedPerspective + "'");
+		PreferencesUtil.setOpenAssociatedPerspective(openAssociatedPerspective);
 	}
 
 	/** 
--- tests/org.jboss.reddeer.direct.test/src/org/jboss/reddeer/direct/test/preferences/PreferencesUtilTest.java
+++ tests/org.jboss.reddeer.direct.test/src/org/jboss/reddeer/direct/test/preferences/PreferencesUtilTest.java
@@ -7,26 +7,42 @@
  * 
  * Contributors: 
  * Red Hat, Inc. - initial API and implementation 
- ******************************************************************************/ 
+ ******************************************************************************/
 package org.jboss.reddeer.direct.test.preferences;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import org.jboss.reddeer.direct.preferences.PreferencesUtil;
+import org.jboss.reddeer.eclipse.ui.dialogs.PerspectivesPreferencePage;
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
 import org.jboss.reddeer.swt.api.Menu;
 import org.jboss.reddeer.swt.impl.menu.ShellMenu;
+import org.jboss.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * Test for core manipulation with common preferences.
  * 
- * @author Andrej Podhradsky
+ * @author Andrej Podhradsky (apodhrad@redhat.com)
  * 
  */
 @RunWith(RedDeerSuite.class)
 public class PreferencesUtilTest {
+
+	private String openAssosiatedPerspective;
+
+	public PreferencesUtilTest() {
+		openAssosiatedPerspective = PreferencesUtil.getOpenAssociatedPerspective();
+	}
+
+	@After
+	public void resetPreferences() {
+		PreferencesUtil.setOpenAssociatedPerspective(openAssosiatedPerspective);
+	}
 
 	@Test
 	public void autoBuildingTest() {
@@ -39,4 +55,62 @@ public class PreferencesUtilTest {
 		assertEquals(menu.isSelected(), true);
 		assertEquals(menu.isSelected(), PreferencesUtil.isAutoBuildingOn());
 	}
+
+	@Test
+	public void getOpenAssociatedPerspectiveTest() {
+		WorkbenchPreferenceDialog preferencesDialog = new WorkbenchPreferenceDialog();
+		PerspectivesPreferencePage perspectivesPreferencePage = new PerspectivesPreferencePage();
+		preferencesDialog.open();
+		preferencesDialog.select(perspectivesPreferencePage);
+
+		perspectivesPreferencePage.checkAlwaysOpenAssociatedPerspective();
+		perspectivesPreferencePage.apply();
+		assertEquals("always", PreferencesUtil.getOpenAssociatedPerspective());
+
+		perspectivesPreferencePage.checkPromptOpenAssociatedPerspective();
+		perspectivesPreferencePage.apply();
+		assertEquals("prompt", PreferencesUtil.getOpenAssociatedPerspective());
+
+		perspectivesPreferencePage.checkNeverOpenAssociatedPerspective();
+		perspectivesPreferencePage.apply();
+		assertEquals("never", PreferencesUtil.getOpenAssociatedPerspective());
+
+		preferencesDialog.cancel();
+	}
+
+	@Test
+	public void setOpenAssociatedPerspectiveTest() {
+		PreferencesUtil.setOpenAssociatedPerspective("always");
+
+		WorkbenchPreferenceDialog preferencesDialog = new WorkbenchPreferenceDialog();
+		PerspectivesPreferencePage perspectivesPreferencePage = new PerspectivesPreferencePage();
+		preferencesDialog.open();
+		preferencesDialog.select(perspectivesPreferencePage);
+		assertTrue(perspectivesPreferencePage.isAlwaysOpenAssociatedPerspective());
+		preferencesDialog.cancel();
+
+		PreferencesUtil.setOpenAssociatedPerspective("prompt");
+		preferencesDialog.open();
+		preferencesDialog.select(perspectivesPreferencePage);
+		assertTrue(perspectivesPreferencePage.isPromptOpenAssociatedPerspective());
+		preferencesDialog.cancel();
+
+		PreferencesUtil.setOpenAssociatedPerspective("never");
+		preferencesDialog.open();
+		preferencesDialog.select(perspectivesPreferencePage);
+		assertTrue(perspectivesPreferencePage.isNeverOpenAssociatedPerspective());
+		preferencesDialog.cancel();
+	}
+
+	@Test
+	public void setOpenAssociatedPerspectiveWithIncorrectOptionTest() {
+		String incorrectOption = "sometimes";
+		try {
+			PreferencesUtil.setOpenAssociatedPerspective(incorrectOption);
+			fail("IllegalArgumentException was expected for the option '" + incorrectOption + "'");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Expected one of [always, never, prompt] but was '" + incorrectOption + "'", e.getMessage());
+		}
+	}
+
 }
