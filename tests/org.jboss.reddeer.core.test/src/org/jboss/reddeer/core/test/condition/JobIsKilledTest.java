8532680e4e511a19b3d203e8b883a47d9961a130
Rastislav Wagner <rawagner@redhat.com>
2016-12-02 10:21:05 +0100

    Fix failing jobkill test

--- tests/org.jboss.reddeer.core.test/src/org/jboss/reddeer/core/test/condition/JobIsKilledTest.java
+++ tests/org.jboss.reddeer.core.test/src/org/jboss/reddeer/core/test/condition/JobIsKilledTest.java
@@ -4,7 +4,9 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.jboss.reddeer.common.condition.AbstractWaitCondition;
 import org.jboss.reddeer.common.exception.WaitTimeoutExpiredException;
+import org.jboss.reddeer.common.wait.AbstractWait;
 import org.jboss.reddeer.common.wait.TimePeriod;
 import org.jboss.reddeer.common.wait.WaitUntil;
 import org.jboss.reddeer.common.wait.WaitWhile;
@@ -24,21 +26,19 @@ import org.junit.runner.RunWith;
 public class JobIsKilledTest {
 
 	public static final String TEST_JOB_NAME = "Test job";
+	public static final String TEST_JOB_EXCEPTION_NAME = "Test job Exception";
 
 	@Test
 	public void testKillJob() {
 		Job testJob = new Job(TEST_JOB_NAME) {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
-				try {
-					for (int i = 0; i < 60; i++) {
-						if (monitor.isCanceled()) {
-							return Status.CANCEL_STATUS;
-						}
-						Thread.sleep(1000);
+				long sleepTime = 60000;
+				long expectedtime = System.currentTimeMillis() + sleepTime;
+				while (System.currentTimeMillis() < expectedtime) {
+					if (monitor.isCanceled()) {
+						return Status.CANCEL_STATUS;
 					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
 				}
 				return Status.OK_STATUS;
 			}
@@ -54,18 +54,16 @@ public class JobIsKilledTest {
 			}
 		}
 	}
-	
+
 	@Test
 	public void testKillJobException() {
-		Job testJob = new Job(TEST_JOB_NAME) {
+		Job testJob = new Job(TEST_JOB_EXCEPTION_NAME) {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
-				try {
-					for (int i = 0; i < 60; i++) {
-						Thread.sleep(1000);
-					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+				long sleepTime = 60000;
+				long expectedtime = System.currentTimeMillis() + sleepTime;
+				while (System.currentTimeMillis() < expectedtime) {
+					// wait
 				}
 				return Status.OK_STATUS;
 			}
@@ -73,12 +71,29 @@ public class JobIsKilledTest {
 		testJob.schedule();
 
 		try {
-			new WaitUntil(new JobIsKilled(TEST_JOB_NAME));
+			new WaitUntil(new JobHasState(testJob, Job.RUNNING));
+			new WaitUntil(new JobIsKilled(TEST_JOB_EXCEPTION_NAME));
 		} catch (WaitTimeoutExpiredException wte) {
 			new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
 			return;
 		}
 		new WaitWhile(new JobIsRunning(), TimePeriod.LONG);
 		Assert.fail("No exception was thrown");
+	}
+	
+	private class JobHasState extends AbstractWaitCondition {
+		
+		private Job job;
+		private int state;
+		
+		public JobHasState(Job job, int state) {
+			this.job = job;
+			this.state = state;
+		}
+
+		@Override
+		public boolean test() {
+			return job.getState() == state;
+		}
 	}
 }
