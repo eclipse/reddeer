713c590900f66cba357c9d1af1146726d8e9e3f6
Rastislav Wagner <rawagner@redhat.com>
2016-12-20 18:30:20 +0100

    Enhance System class to be able to accept ssh keys. Fixes #1556

--- plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/rse/ui/wizard/SystemPasswordPromptDialog.java
+++ plugins/org.jboss.reddeer.eclipse/src/org/jboss/reddeer/eclipse/rse/ui/wizard/SystemPasswordPromptDialog.java
@@ -10,34 +10,47 @@
  ******************************************************************************/ 
 package org.jboss.reddeer.eclipse.rse.ui.wizard;
 
-import org.jboss.reddeer.swt.api.Shell;
+import org.jboss.reddeer.common.wait.WaitWhile;
+import org.jboss.reddeer.swt.condition.ShellIsAvailable;
+import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
 
 /**
  * This class represents Remote System password prompt dialog
  * @author Pavol Srna
  *
  */
-public class SystemPasswordPromptDialog {
+public class SystemPasswordPromptDialog extends DefaultShell{
 
 	public static final String TITLE = "Enter Password";
-	
-	private Shell shell;
 	
 	/**
 	 * Constructs a dialog with {@value #TITLE}.
 	 */
 	public SystemPasswordPromptDialog() {
-		shell = new DefaultShell(TITLE);
+		super(TITLE);
 	}
 	
-	/**
-	 * Get Shell of the Remote System password prompt dialog.
-	 *
-	 * @return Shell
-	 */
-	public Shell getShell(){
-		return this.shell;
+	public void setUserID(String username){
+		new LabeledText("User ID:").setText(username);
+	}
+	
+	public void setPassword(String password){
+		new LabeledText("Password (optional):").setText(password);
+	}
+	
+	public String getUserID(){
+		return new LabeledText("User ID:").getText();
+	}
+	
+	public String getPassword(){
+		return new LabeledText("Password (optional):").getText();
+	}
+	
+	public void OK(){
+		new PushButton("OK").click();
+		new WaitWhile(new ShellIsAvailable(this));
 	}
 	
 }
