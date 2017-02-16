4db48123cca5bd0d88242e75608e78d0037f5c80
Rastislav Wagner <rawagner@redhat.com>
2017-02-16 16:35:18 +0100

    Static requirement doesn't work in parameterized tests. Fixes #1476

---
+++ tests/org.jboss.reddeer.junit.test/src/org/jboss/reddeer/junit/test/internal/requirement/parameterized/inject/ParameterizedRequirementTest.java
@@ -0,0 +1,30 @@
+/******************************************************************************* 
+ * Copyright (c) 2017 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package org.jboss.reddeer.junit.test.internal.requirement.parameterized.inject;
+
+import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.junit.Test;
+import org.junit.internal.builders.AllDefaultPossibilitiesBuilder;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.model.InitializationError;
+import static org.junit.Assert.*;
+
+public class ParameterizedRequirementTest {
+
+	@Test
+	public void testStaticInject() throws InitializationError {
+		RunNotifier notifier = new RunNotifier();
+		RedDeerSuite rs = new RedDeerSuite(ParameterizedStaticReqTestClazz.class, new AllDefaultPossibilitiesBuilder(true));
+		rs.run(notifier);
+		assertTrue(ParameterizedStaticReqTestClazz.getReq() != null);
+	}
+
+}
