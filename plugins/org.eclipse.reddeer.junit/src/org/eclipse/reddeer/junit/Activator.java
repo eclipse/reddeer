b6a5448c2cf7347363cd5c3185b372dee6fdc61f
Rastislav Wagner <rawagner@redhat.com>
2017-07-17 10:09:52 +0200

    Register requirements via extension point. Fixes #1747
    Signed-off-by: Rastislav Wagner <rawagner@redhat.com>

--- plugins/org.eclipse.reddeer.junit/src/org/eclipse/reddeer/junit/Activator.java
+++ plugins/org.eclipse.reddeer.junit/src/org/eclipse/reddeer/junit/Activator.java
@@ -10,6 +10,14 @@
  *******************************************************************************/
 package org.eclipse.reddeer.junit;
 
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.reddeer.common.logging.Logger;
+import org.eclipse.reddeer.junit.requirement.Requirement;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -20,9 +28,13 @@ public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.eclipse.reddeer.junit"; //$NON-NLS-1$
+	public static final String REQUIREMENTS_EXTENSION_POINT="org.eclipse.reddeer.junit.requirement";
+	private static final Logger log = Logger.getLogger(Activator.class);
 
 	// The shared instance
 	private static Activator plugin;
+	
+	private static List<Requirement<Annotation>> requirements;
 	
 	/**
 	 * The constructor.
@@ -55,6 +67,23 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public static Activator getDefault() {
 		return plugin;
+	}
+	
+	public static List<Requirement<Annotation>> getRequirements(){
+		if(requirements == null){
+			requirements = new ArrayList<>();
+			IConfigurationElement[] elements = Platform.getExtensionRegistry().getConfigurationElementsFor(REQUIREMENTS_EXTENSION_POINT);
+			for (IConfigurationElement e : elements) {
+				try {
+					log.info("Found requirement "+e.getAttribute("class"));
+					final Requirement<Annotation> requirement = (Requirement<Annotation>)e.createExecutableExtension("class");
+					requirements.add(requirement);
+				} catch (Exception ex) {
+					log.error("Unable to instantiate requirement "+e.getAttribute("class"));
+				}
+			}
+		}
+		return requirements;
 	}
 
 }
