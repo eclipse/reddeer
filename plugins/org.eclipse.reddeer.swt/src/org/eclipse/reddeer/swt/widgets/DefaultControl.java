898949aab5f973a7236aeb8dc82b967406704469
Lukáš Valach <lvalach@redhat.com>
2018-02-23 12:37:38 +0100

    REDDEER-1845 Implement default classes for swt.Widget and swt.Control
    Signed-off-by: Lukáš Valach <lvalach@redhat.com>

---
+++ plugins/org.eclipse.reddeer.swt/src/org/eclipse/reddeer/swt/widgets/DefaultControl.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.reddeer.swt.widgets;
+
+import org.eclipse.reddeer.core.reference.ReferencedComposite;
+import org.hamcrest.Matcher;
+
+/**
+ * Default class for all control 
+ * @author lvalach
+ *
+ * @param <T> extends swt control
+ */
+public class DefaultControl<T extends org.eclipse.swt.widgets.Control> extends AbstractControl<T> {
+	
+	/**
+	 * Finds control of specified class, within referenced composite with defined index and matching all matchers
+	 * @param controlClass class of control
+	 * @param refComposite composite where control should be found
+	 * @param index of control
+	 * @param matchers matchers matching control
+	 */
+	public DefaultControl(Class<T> controlClass, ReferencedComposite refComposite, int index, Matcher<?>... matchers) {
+		super(controlClass,refComposite,index,matchers);
+	}
+
+	/**
+	 * Constructs reddeer control with specified swt control
+	 * @param swtControl instance of swt control
+	 */
+	public DefaultControl(T swtControl) {
+		super(swtControl);
+	}
+	
+	/**
+	 * Constructs reddeer control with specified swt control, if control is null diagnostics runnable is run 
+	 * (to provide additional debug output etc)
+	 * @param swtControl instance of swt control
+	 * @param diagnostics diagnostics runnable to run if control is null
+	 */
+	public DefaultControl(T swtControl, Runnable diagnostics) {
+		super(swtControl, diagnostics);
+	}
+}
---
+++ tests/org.eclipse.reddeer.swt.test/src/org/eclipse/reddeer/swt/test/widgets/DefaultControlTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.reddeer.swt.test.widgets;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.reddeer.junit.runner.RedDeerSuite;
+import org.eclipse.reddeer.swt.impl.button.PushButton;
+import org.eclipse.reddeer.swt.impl.shell.DefaultShell;
+import org.eclipse.reddeer.swt.widgets.DefaultControl;
+import org.eclipse.swt.widgets.Button;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(RedDeerSuite.class)
+public class DefaultControlTest extends DefaultWidgetAndControlBase{
+
+	@Test
+	public void defaultControlTest() {
+		DefaultShell shell = new DefaultShell(SHELL_TITLE);
+		PushButton button = new PushButton(shell);
+
+		DefaultControl<Button> defaultControl = new DefaultControl<Button>(this.control);
+		assertEquals(defaultControl.getSWTWidget(), button.getSWTWidget());
+	}
+}
