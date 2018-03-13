0145eeb9ac24b74074744c197215ff6a8b25f9f6
Lukáš Valach <lvalach@redhat.com>
2018-03-13 08:38:28 +0100

    REDDEE-1835 Implement a dialog for filtering Java classes
    Signed-off-by: Lukáš Valach <lvalach@redhat.com>

---
+++ plugins/org.eclipse.reddeer.eclipse/src/org/eclipse/reddeer/eclipse/jdt/ui/dialogs/OpenTypeSelectionDialog.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.reddeer.eclipse.jdt.ui.dialogs;
+
+import org.eclipse.reddeer.eclipse.ui.dialogs.FilteredItemsSelectionDialog;
+import org.eclipse.reddeer.jface.window.AbstractWindow;
+import org.eclipse.reddeer.jface.window.Openable;
+import org.eclipse.reddeer.swt.api.Shell;
+import org.eclipse.reddeer.workbench.workbenchmenu.WorkbenchMenuOpenable;
+
+/**
+ * Represents {@value #DIALOG_TITLE} dialog.
+ * 
+ * @see org.eclipse.jdt.internal.ui.dialogs.OpenTypeSelectionDialog
+ * 
+ * @author lvalach
+ *
+ */
+public class OpenTypeSelectionDialog extends FilteredItemsSelectionDialog {
+
+	public static final String DIALOG_TITLE = "Open Type";
+	public static final String[] MENU_PATH = new String[] { "Navigate", "Open Type..." };
+
+	/**
+	 * Instantiates new OpenTypeSelectionDialog. Implementations are responsible for
+	 * making sure given shell is OpenTypeSelectionDialog.
+	 * 
+	 * @param shell
+	 *            instance of OpenTypeSelectionDialog
+	 */
+	public OpenTypeSelectionDialog(Shell shell) {
+		super(shell);
+	}
+
+	/**
+	 * Instantiates new OpenTypeSelectionDialog. An {@value #DIALOG_TITLE} shell
+	 * will be connected with this instance. If there is no such shell, the shell
+	 * may be assigned additionally (see {@link AbstractWindow#activate()}).
+	 */
+	public OpenTypeSelectionDialog() {
+		super();
+		isOpen();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Openable getDefaultOpenAction() {
+		return new WorkbenchMenuOpenable(DIALOG_TITLE, MENU_PATH);
+	}
+}
---
+++ plugins/org.eclipse.reddeer.eclipse/src/org/eclipse/reddeer/eclipse/ui/dialogs/FilteredItemsSelectionDialog.java
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat, Inc and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.reddeer.eclipse.ui.dialogs;
+
+import org.eclipse.reddeer.common.wait.WaitWhile;
+import org.eclipse.reddeer.core.matcher.WithMnemonicTextMatcher;
+import org.eclipse.reddeer.jface.window.AbstractWindow;
+import org.eclipse.reddeer.jface.window.Openable;
+import org.eclipse.reddeer.swt.api.Shell;
+import org.eclipse.reddeer.swt.api.Table;
+import org.eclipse.reddeer.swt.impl.button.CancelButton;
+import org.eclipse.reddeer.swt.impl.button.OkButton;
+import org.eclipse.reddeer.swt.impl.table.DefaultTable;
+import org.eclipse.reddeer.swt.impl.text.DefaultText;
+import org.eclipse.reddeer.workbench.core.condition.JobIsRunning;
+import org.eclipse.ui.internal.WorkbenchMessages;
+import org.hamcrest.Matcher;
+
+/**
+ * Represends {@link org.eclipse.ui.dialogs.FilteredItemsSelectionDialog}.
+ * 
+ * @author lvalach
+ *
+ */
+public class FilteredItemsSelectionDialog extends AbstractWindow {
+
+	/**
+	 * Implementations are responsible for making sure given shell is
+	 * FilteredItemsSelectionDialog.
+	 * 
+	 * @param shell
+	 *            instance of FilteredItemsSelectionDialog
+	 */
+	public FilteredItemsSelectionDialog(Shell shell) {
+		super(shell);
+	}
+
+	/**
+	 * Finds FilteredItemsSelectionDialog matching given matchers.
+	 * 
+	 * @param matchers
+	 *            to match FilteredItemsSelectionDialog
+	 */
+	public FilteredItemsSelectionDialog(Matcher<?>... matchers) {
+		super(matchers);
+	}
+
+	/**
+	 * Shell won't be assigned at the moment of construction but may be assigned
+	 * additionally (see {@link AbstractWindow#activate()}).
+	 */
+	public FilteredItemsSelectionDialog() {
+		super();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Openable getDefaultOpenAction() {
+		return null;
+	}
+
+	/**
+	 * Click 'OK' button.
+	 */
+	public void ok() {
+		new OkButton(this).click();
+	}
+
+	/**
+	 * Click 'Cancel' button.
+	 */
+	public void cancel() {
+		new CancelButton(this).click();
+	}
+
+	/**
+	 * Get filter expression.
+	 * 
+	 * @return text from search box
+	 */
+	public void getFilterText() {
+		new DefaultText(this).getText();
+	}
+
+	/**
+	 * Set filter expression.
+	 */
+	@SuppressWarnings("restriction")
+	public void setFilterText(String text) {
+		new DefaultText(this).setText(text);
+		new WaitWhile(new JobIsRunning(
+				new WithMnemonicTextMatcher(WorkbenchMessages.FilteredItemsSelectionDialog_jobLabel), false), false);
+	}
+
+	/**
+	 * Get results table.
+	 * 
+	 * @return table containing results of the search
+	 */
+	public Table getResultsTable() {
+		return new DefaultTable(this);
+	}
+
+	/**
+	 * Selects items in results table.
+	 * 
+	 * @param items
+	 *            texts of items to select
+	 * @see {@link Table#select(String...)}
+	 */
+	public void selectItem(String... items) {
+		new DefaultTable(this).select(items);
+	}
+
+	/**
+	 * Selects items in results table.
+	 * 
+	 * @param indices
+	 *            row indices to select
+	 * @see {@link Table#select(int...)}
+	 */
+	public void selectItem(int... indices) {
+		new DefaultTable(this).select(indices);
+	}
+}
