fb4cc63d54e71effa52dd04ebe4399b58776d2a0
Rastislav Wagner <rawagner@redhat.com>
2018-05-16 14:47:03 +0200

    Remove breakpoints button changed text in Photon. Fixes #1923
    Signed-off-by: Rastislav Wagner <rawagner@redhat.com>

--- plugins/org.eclipse.reddeer.eclipse/src/org/eclipse/reddeer/eclipse/debug/ui/views/breakpoints/BreakpointsView.java
+++ plugins/org.eclipse.reddeer.eclipse/src/org/eclipse/reddeer/eclipse/debug/ui/views/breakpoints/BreakpointsView.java
@@ -12,18 +12,27 @@ package org.eclipse.reddeer.eclipse.debug.ui.views.breakpoints;
 
 import java.util.List;
 
+import org.eclipse.reddeer.common.condition.AbstractWaitCondition;
 import org.eclipse.reddeer.common.wait.AbstractWait;
 import org.eclipse.reddeer.common.wait.TimePeriod;
 import org.eclipse.reddeer.common.wait.WaitUntil;
 import org.eclipse.reddeer.common.wait.WaitWhile;
+import org.eclipse.reddeer.core.condition.WidgetIsFound;
+import org.eclipse.reddeer.core.matcher.WithMnemonicTextMatcher;
+import org.eclipse.reddeer.core.matcher.WithTextMatcher;
+import org.eclipse.reddeer.eclipse.exception.EclipseLayerException;
+import org.eclipse.reddeer.eclipse.ui.dialogs.FilteredItemsSelectionDialog;
+import org.eclipse.reddeer.jface.wizard.WizardDialog;
+import org.eclipse.reddeer.swt.api.Button;
+import org.eclipse.reddeer.swt.api.Shell;
+import org.eclipse.reddeer.swt.api.ToolItem;
 import org.eclipse.reddeer.swt.api.TreeItem;
 import org.eclipse.reddeer.swt.condition.ControlIsEnabled;
 import org.eclipse.reddeer.swt.condition.ShellIsAvailable;
 import org.eclipse.reddeer.swt.impl.button.OkButton;
 import org.eclipse.reddeer.swt.impl.button.PushButton;
-import org.eclipse.reddeer.swt.impl.menu.ContextMenuItem;
+import org.eclipse.reddeer.swt.impl.button.YesButton;
 import org.eclipse.reddeer.swt.impl.shell.DefaultShell;
-import org.eclipse.reddeer.swt.impl.text.DefaultText;
 import org.eclipse.reddeer.swt.impl.text.LabeledText;
 import org.eclipse.reddeer.swt.impl.toolbar.DefaultToolItem;
 import org.eclipse.reddeer.swt.impl.tree.DefaultTree;
@@ -54,11 +63,10 @@ public class BreakpointsView extends WorkbenchView {
 		log.info("Adding java exception breakpoint '" + exception + "'");
 		open();
 		new DefaultToolItem(cTabItem.getFolder(), "Add Java Exception Breakpoint").click();
-		new DefaultShell("Add Java Exception Breakpoint");
-		new DefaultText().setText(exception);
-		new WaitUntil(new ControlIsEnabled(new OkButton()), TimePeriod.LONG);
-		new OkButton().click();
-		new WaitWhile(new ShellIsAvailable("Add Java Exception Breakpoint"));
+		FilteredItemsSelectionDialog breakpointsDialog = new FilteredItemsSelectionDialog(new WithTextMatcher("Add Java Exception Breakpoint"));
+		breakpointsDialog.setFilterText(exception);
+		new WaitUntil(new ControlIsEnabled(new OkButton(breakpointsDialog)), TimePeriod.LONG);
+		breakpointsDialog.ok();
 	};
 	
 	/**
@@ -83,7 +91,7 @@ public class BreakpointsView extends WorkbenchView {
 		log.info("Accessing breakpoints in Breakpoints view");
 		open();
 		AbstractWait.sleep(TimePeriod.SHORT);
-		List<TreeItem> items = new DefaultTree(cTabItem).getItems();
+		List<TreeItem> items = new DefaultTree(this).getItems();
 		for (TreeItem item : items) {
 			log.debug("\tfound: " + item.getText());
 			if (item.getText().contains(label)) {
@@ -99,10 +107,22 @@ public class BreakpointsView extends WorkbenchView {
 	public void removeAllBreakpoints() {
 		log.info("Removing all breakpoints from Breakpoints view");
 		open();
-		if (new DefaultToolItem(cTabItem.getFolder(), "Remove All Breakpoints").isEnabled()) {
-			new DefaultToolItem(cTabItem.getFolder(), "Remove All Breakpoints").click();
-			new DefaultShell("Remove All Breakpoints").setFocus();
-			new PushButton("Yes").click();
+		ToolItem removeBreakpointsItem = new DefaultToolItem(cTabItem.getFolder(), "Remove All Breakpoints");
+		if (removeBreakpointsItem.isEnabled()) {
+			removeBreakpointsItem.click();
+			Shell removeBreakpointsShell = new DefaultShell("Remove All Breakpoints");
+			
+			//photon changed button text
+			WidgetIsFound removeButtonTest = new WidgetIsFound(org.eclipse.swt.widgets.Button.class, removeBreakpointsShell.getControl(),
+					new WithMnemonicTextMatcher("Remove"));
+			Button button;
+			if(removeButtonTest.test()){
+				button = new PushButton((org.eclipse.swt.widgets.Button)removeButtonTest.getResult());
+			} else {
+				button = new YesButton(removeBreakpointsShell);	
+			}
+			button.click();
+			new WaitWhile(new ShellIsAvailable(removeBreakpointsShell));
 		}
 	}
 
@@ -114,11 +134,21 @@ public class BreakpointsView extends WorkbenchView {
 	public void importBreakpoints(String path) {
 		log.info("Importing breakpoints from '" + path + "'");
 		open();
-		new DefaultTree(cTabItem).setFocus();
-		new ContextMenuItem("Import Breakpoints...").select();
-		new DefaultShell("Import Breakpoints");
-		new LabeledText("From file:").setText(path);
-		new PushButton("Finish").click();
-		new WaitWhile(new ShellIsAvailable("Import Breakpoints"));
+		new DefaultTree(this).getContextMenu().getItem("Import Breakpoints...").select();
+		WizardDialog importBreakpointsDialog = new WizardDialog("Import Breakpoints");
+		importBreakpointsDialog.isOpen();
+		new LabeledText(importBreakpointsDialog, "From file:").setText(path);
+		new WaitUntil(new AbstractWaitCondition() {
+			
+			@Override
+			public boolean test() {
+				return importBreakpointsDialog.isFinishEnabled();
+			}
+		}, TimePeriod.SHORT, false);
+		if(!importBreakpointsDialog.isFinishEnabled()) {
+			throw new EclipseLayerException("Finish button is not enabled. Dialog error is '"
+					+importBreakpointsDialog.getMessage()+"'");
+		}
+		importBreakpointsDialog.finish();
 	}
 }
