28c77985aea3c24c2f418695f87bd003f44e7cc6
Ondrej Dockal <odockal@redhat.com>
2019-06-12 10:14:32 +0200

    Adding missing copyright
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- plugins/org.eclipse.reddeer.eclipse/src/org/eclipse/reddeer/eclipse/jdt/ui/wizards/AbstractJavaWizardPage.java
+++ plugins/org.eclipse.reddeer.eclipse/src/org/eclipse/reddeer/eclipse/jdt/ui/wizards/AbstractJavaWizardPage.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat, Inc and others.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Red Hat, Inc - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.reddeer.eclipse.jdt.ui.wizards;
 
 import org.eclipse.reddeer.common.exception.RedDeerException;
