ad3bbd219fffec5b5d0acc0cdf90e09d773ef54d
Oleksii Korniienko <olkornii@redhat.com>
2021-05-17 10:06:00 +0200

    Created QuarkusLaunchConfiguration class
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

---
+++ plugins/org.eclipse.reddeer.eclipse/src/org/eclipse/reddeer/eclipse/debug/ui/launchConfigurations/QuarkusLaunchConfiguration.java
@@ -0,0 +1,24 @@
+/******************************************************************************* 
+ * Copyright (c) 2021 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/
+package org.eclipse.reddeer.eclipse.debug.ui.launchConfigurations;
+/**
+ * 
+ * @author Oleksii Korniienko olkornii@redhat.com
+ *
+ */
+
+public class QuarkusLaunchConfiguration extends LaunchConfiguration {
+
+	public QuarkusLaunchConfiguration() {
+		super("Quarkus Application");
+	}
+
+}
