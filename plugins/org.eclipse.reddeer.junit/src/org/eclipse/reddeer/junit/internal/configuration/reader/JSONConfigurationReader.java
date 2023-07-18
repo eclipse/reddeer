a1298a59161d947da36f4a747eb94c2181443aac
Ondrej Dockal <odockal@redhat.com>
2023-07-18 19:08:11 +0200

    Update snakeyaml to the latest version available in oompt update site. fixes #2198
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- plugins/org.eclipse.reddeer.junit/src/org/eclipse/reddeer/junit/internal/configuration/reader/JSONConfigurationReader.java
+++ plugins/org.eclipse.reddeer.junit/src/org/eclipse/reddeer/junit/internal/configuration/reader/JSONConfigurationReader.java
@@ -29,8 +29,9 @@ import org.eclipse.reddeer.junit.requirement.Requirement;
 import org.eclipse.reddeer.junit.requirement.RequirementException;
 import org.eclipse.reddeer.junit.requirement.configuration.RequirementConfiguration;
 import org.json.JSONObject;
+import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.SafeConstructor;
+import org.yaml.snakeyaml.constructor.Constructor;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -86,7 +87,7 @@ public class JSONConfigurationReader implements ConfigurationReader {
 	
 	@SuppressWarnings("unchecked")
 	private String convertYamlToJson(File file) throws FileNotFoundException {
-		Map<String,Object> map= (Map<String, Object>) new Yaml(new SafeConstructor()).load(new FileReader(file));
+		Map<String,Object> map= (Map<String, Object>) new Yaml(new Constructor(new LoaderOptions())).load(new FileReader(file));
 		JSONObject jsonObject = new JSONObject(map);
 		return jsonObject.toString();
 	}
