247acea707a3f77c18fcbddc2ed83c73b2a4a25e
Oleksii Korniienko <olkornii@redhat.com>
2023-09-05 15:29:32 +0200

    Fixed "Do not automatically update..." because check box has been removed. Added test for Global Update Policy.
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- tests/org.eclipse.reddeer.eclipse.test/src/org/eclipse/reddeer/eclipse/test/m2e/core/ui/preferences/MavenPreferencePageTest.java
+++ tests/org.eclipse.reddeer.eclipse.test/src/org/eclipse/reddeer/eclipse/test/m2e/core/ui/preferences/MavenPreferencePageTest.java
@@ -27,14 +27,33 @@ public class MavenPreferencePageTest {
 	private WorkbenchPreferenceDialog preferencesDialog = new WorkbenchPreferenceDialog();
 	
 	private MavenPreferencePage mavenPreferencePage = new MavenPreferencePage(preferencesDialog);
+	
+	@Test
+	public void checkGlobalUpdatePolicy() {
+		preferencesDialog.open();
+		preferencesDialog.select(mavenPreferencePage);
 
+		mavenPreferencePage.setGlobalUpdatePolicy("Always");
+		assertTrue("Always".equals(mavenPreferencePage.getGlobalUpdatePolicy()));
+		mavenPreferencePage.setGlobalUpdatePolicy("Daily");
+		assertTrue("Daily".equals(mavenPreferencePage.getGlobalUpdatePolicy()));
+		mavenPreferencePage.setGlobalUpdatePolicy("Hourly");
+		assertTrue("Hourly".equals(mavenPreferencePage.getGlobalUpdatePolicy()));;
+		mavenPreferencePage.setGlobalUpdatePolicy("Default");
+		assertTrue("Default".equals(mavenPreferencePage.getGlobalUpdatePolicy()));
+		mavenPreferencePage.setGlobalUpdatePolicy("Never");
+		assertTrue("Never".equals(mavenPreferencePage.getGlobalUpdatePolicy()));
+
+		preferencesDialog.cancel();
+	}
+	
 	@Test
 	public void checkAllPreferences() {
 		preferencesDialog.open();
 		preferencesDialog.select(mavenPreferencePage);
 		
 		mavenPreferencePage.setDebugOutput(true);
-//		mavenPreferencePage.setDoNotAutoUpdateDeps(true); # is not a part of 2023-09 Eclipse
+		mavenPreferencePage.setDoNotAutoUpdateDeps(true);
 		mavenPreferencePage.setDownloadArtifactJavadoc(true);
 		mavenPreferencePage.setDownloadArtifactSources(true);
 		mavenPreferencePage.setDownloadRepoIndexOnStartup(true);
@@ -43,14 +62,14 @@ public class MavenPreferencePageTest {
 		mavenPreferencePage.setUpdateMavenProjectsOnStartup(true);
 		
 		assertTrue(mavenPreferencePage.isDebugOutputChecked());
-//		assertTrue(mavenPreferencePage.isDoNotAutoUpdateDepsChecked()); # is not a part of 2023-09 Eclipse
+		assertTrue(mavenPreferencePage.isDoNotAutoUpdateDepsChecked());
 		assertTrue(mavenPreferencePage.isDownloadArtifactJavadocChecked());
 		assertTrue(mavenPreferencePage.isDownloadArtifactSourcesChecked());
 		assertTrue(mavenPreferencePage.isDownloadRepoIndexOnStartupChecked());
 		assertTrue(mavenPreferencePage.isHideFoldersOfPhysicalyNestedModulesChecked());
 		assertTrue(mavenPreferencePage.isOfflineChecked());
 		assertTrue(mavenPreferencePage.isUpdateMavenProjectsOnStartupChecked());
-		
+
 		preferencesDialog.cancel();
 	}
 
@@ -60,7 +79,7 @@ public class MavenPreferencePageTest {
 		preferencesDialog.select(mavenPreferencePage);
 		
 		mavenPreferencePage.setDebugOutput(false);
-//		mavenPreferencePage.setDoNotAutoUpdateDeps(false); # is not a part of 2023-09 Eclipse
+		mavenPreferencePage.setDoNotAutoUpdateDeps(false);
 		mavenPreferencePage.setDownloadArtifactJavadoc(false);
 		mavenPreferencePage.setDownloadArtifactSources(false);
 		mavenPreferencePage.setDownloadRepoIndexOnStartup(false);
@@ -69,7 +88,7 @@ public class MavenPreferencePageTest {
 		mavenPreferencePage.setUpdateMavenProjectsOnStartup(false);
 		
 		assertFalse(mavenPreferencePage.isDebugOutputChecked());
-//		assertFalse(mavenPreferencePage.isDoNotAutoUpdateDepsChecked()); # is not a part of 2023-09 Eclipse
+		assertFalse(mavenPreferencePage.isDoNotAutoUpdateDepsChecked());
 		assertFalse(mavenPreferencePage.isDownloadArtifactJavadocChecked());
 		assertFalse(mavenPreferencePage.isDownloadArtifactSourcesChecked());
 		assertFalse(mavenPreferencePage.isDownloadRepoIndexOnStartupChecked());
